9677ecc97c59cbefed77fad1cb5a85b78e716973
Derek G Foster <derek.foster@intel.com>
2022-12-11 10:36:56 -0800

    - Addressed several issues found by shellcheck. - Made a couple of edits for readability and consistency.
    Signed-off-by: Derek G Foster <derek.foster@intel.com>

--- scripts/dpdk/setup_env.sh
+++ scripts/dpdk/setup_env.sh
@@ -1,25 +1,23 @@
-#Copyright (C) 2021-2022 Intel Corporation
-#SPDX-License-Identifier: Apache-2.0
-
 #! /bin/bash
+
+# Copyright (C) 2021-2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
 set -e
 
-if [ -z "$1" ]
-then
+if [ -z "$1" ]; then
     echo "- Missing mandatory argument: IPDK_RECIPE"
     echo " - Usage: source setup_env.sh <IPDK_RECIPE> <SDE_INSTALL> <DEPEND_INSTALL>"
     return 0
 fi
 
-if [ -z "$2" ]
-then
+if [ -z "$2" ]; then
     echo "- Missing mandatory argument: SDE_INSTALL"
     echo " - Usage: source setup_env.sh <IPDK_RECIPE> <SDE_INSTALL> <DEPEND_INSTALL>"
     return 0
 fi
 
-if [ -z "$3" ]
-then
+if [ -z "$3" ]; then
     echo "- Missing mandatory argument: DEPEND_INSTALL"
     echo " - Usage: source setup_env.sh <IPDK_RECIPE> <SDE_INSTALL> <DEPEND_INSTALL>"
     return 0
@@ -29,29 +27,30 @@ export IPDK_RECIPE=$1
 export SDE_INSTALL=$2
 export DEPEND_INSTALL=$3
 
-#Get the OS and Version details
-. $IPDK_RECIPE/scripts/dpdk/os_ver_details.sh
+# Get the OS and Version details
+# shellcheck source=/dev/null
+. "$IPDK_RECIPE/scripts/dpdk/os_ver_details.sh"
 get_os_ver_details
 echo "OS and Version details..."
 echo "$OS : $VER"
 
-#Update SDE libraries
+# Update SDE libraries
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SDE_INSTALL/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SDE_INSTALL/lib64
 
-if [ $OS = "Fedora" ]; then
+if [ "$OS" = "Fedora" ]; then
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SDE_INSTALL/lib64
 else
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SDE_INSTALL/lib/x86_64-linux-gnu
 fi
 
-#Update IPDK RECIPE libraries
+# Update IPDK RECIPE libraries
 export LD_LIBRARY_PATH=$IPDK_RECIPE/install/lib:$LD_LIBRARY_PATH
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib64
 
-#Update Dependent libraries
+# Update Dependent libraries
 export LD_LIBRARY_PATH=$DEPEND_INSTALL/lib:$DEPEND_INSTALL/lib64:$LD_LIBRARY_PATH
 export PATH=$DEPEND_INSTALL/bin:$DEPEND_INSTALL/sbin:$PATH
 export LIBRARY_PATH=$DEPEND_INSTALL/lib:$DEPEND_INSTALL/lib64:$LIBRARY_PATH
