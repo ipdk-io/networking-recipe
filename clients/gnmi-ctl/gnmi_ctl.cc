741ba01712c217084b4f5e252e9c32f3066ca131
Sabeel Ansari <sabeel.ansari@intel.com>
2023-01-11 22:40:37 -0800

    Update gnmi-ctl with only the connection portion for tls-mode
    Signed-off-by: Sabeel Ansari <sabeel.ansari@intel.com>

--- clients/gnmi-ctl/gnmi_ctl.cc
+++ clients/gnmi-ctl/gnmi_ctl.cc
@@ -1,14 +1,17 @@
 // Copyright 2019-present Open Networking Foundation
+// Copyright 2021-2022 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
 #include <csignal>
 #include <iostream>
 #include <memory>
+#include <regex>  // NOLINT
 #include <string>
 #include <vector>
 
 #include "absl/cleanup/cleanup.h"
 #include "gflags/gflags.h"
+#include "gnmi_ctl_utils.h"
 #include "gnmi/gnmi.grpc.pb.h"
 #include "grpcpp/grpcpp.h"
 #include "grpcpp/security/credentials.h"
@@ -24,18 +27,32 @@
 
 #define DEFAULT_CERTS_DIR "/usr/share/stratum/certs/"
 
-DEFINE_string(grpc_addr, stratum::kLocalStratumUrl, "gNMI server address");
-DEFINE_string(bool_val, "", "Boolean value to be set");
-DEFINE_string(int_val, "", "Integer value to be set (64-bit)");
-DEFINE_string(uint_val, "", "Unsigned integer value to be set (64-bit)");
-DEFINE_string(string_val, "", "String value to be set");
-DEFINE_string(float_val, "", "Floating point value to be set");
-DEFINE_string(bytes_val_file, "", "A file to be sent as bytes value");
-DEFINE_string(proto_bytes, "", "Floating point value to be set");
+const char kUsage[] =
+    R"USAGE(usage: gnmi-ctl [Options] {get,set,cap,sub-onchange,sub-sample} parameters
 
-DEFINE_uint64(interval, 5000, "Subscribe poll interval in ms");
-DEFINE_bool(replace, false, "Use replace instead of update");
-DEFINE_string(get_type, "ALL", "The gNMI get request type");
+Basic gNMI CLI
+
+positional arguments:
+  {get,set,cap,sub-onchange,sub-sample}         gNMI command
+  parameter                                     gNMI config parameters
+
+parameters:
+  "device:<type>,name=<name>,<key:value>"
+
+example:
+   gnmi-ctl set "device:<type>,name=<name>,<key:value>,<key:value>,..."
+   gnmi-ctl get "device:<type>,name=<name>,key"
+)USAGE";
+
+// Pipe file descriptors used to transfer signals from the handler to the cancel
+// function.
+int pipe_read_fd_ = -1;
+int pipe_write_fd_ = -1;
+
+// Pointer to the client context to cancel the blocking calls.
+grpc::ClientContext* ctx_ = nullptr;
+
+#define GNMI_PREDICT_ERROR(x) (__builtin_expect(false || (x), false))
 
 #define PRINT_MSG(msg, prompt)                   \
   do {                                           \
@@ -55,41 +72,6 @@ DEFINE_string(get_type, "ALL", "The gNMI get request type");
       return _status;                                                        \
     }                                                                        \
   } while (0)
-
-namespace gnmi {
-namespace {
-
-using namespace stratum;
-
-const char kUsage[] =
-    R"USAGE(usage: gnmi_cli [--help] [Options] {get,set,cap,del,sub-onchange,sub-sample} path
-
-Basic gNMI CLI
-
-positional arguments:
-  {get,set,cap,del,sub-onchange,sub-sample}         gNMI command
-  path                                              gNMI path
-
-optional arguments:
-  --grpc_addr GRPC_ADDR    gNMI server address
-  --bool_val BOOL_VAL      [SetRequest only] Set boolean value
-  --int_val INT_VAL        [SetRequest only] Set int value (64-bit)
-  --uint_val UINT_VAL      [SetRequest only] Set uint value (64-bit)
-  --string_val STRING_VAL  [SetRequest only] Set string value
-  --float_val FLOAT_VAL    [SetRequest only] Set float value
-  --bytes_val_file FILE    [SetRequest only] A file to be sent as bytes value
-  --interval INTERVAL      [Sample subscribe only] Sample subscribe poll interval in ms
-  --replace                [SetRequest only] Use replace instead of update
-  --get-type               [GetRequest only] Use specific data type for get request (ALL,CONFIG,STATE,OPERATIONAL)
-)USAGE";
-
-// Pipe file descriptors used to transfer signals from the handler to the cancel
-// function.
-int pipe_read_fd_ = -1;
-int pipe_write_fd_ = -1;
-
-// Pointer to the client context to cancel the blocking calls.
-grpc::ClientContext* ctx_ = nullptr;
 
 void HandleSignal(int signal) {
   static_assert(sizeof(signal) <= PIPE_BUF,
@@ -118,34 +100,54 @@ void* ContextCancelThreadFunc(void*) {
   return nullptr;
 }
 
-bool StringToBool(std::string str) {
-  return (str == "y") || (str == "true") || (str == "t") || (str == "yes") ||
-         (str == "1");
-}
+#define MAX_STR_LENGTH 128
 
-void AddPathElem(std::string elem_name, std::string elem_kv,
-                 ::gnmi::PathElem* elem) {
+// FIXME: For now it is connecting to localhost and later it has to be fixed
+// to support any host, by providing a CLI option to the user.
+DEFINE_string(grpc_addr, "localhost:9339", "gNMI server address");
+
+DEFINE_uint64(interval, 5000, "Subscribe poll interval in ms");
+DEFINE_bool(replace, false, "Use replace instead of update");
+DEFINE_string(get_type, "ALL", "The gNMI get request type");
+
+DEFINE_string(root_node, "/interfaces/", "The gNMI root node name");
+DEFINE_string(device_key, "device", "The gNMI cli device key");
+DEFINE_string(device_type_virtual_interface, "virtual-interface",
+              "The gNMI cli device type");
+
+DEFINE_string(name_key, "name", "The gNMI cli name key");
+DEFINE_string(subtree_config, "config", "The gNMI path subtree of type config");
+
+namespace gnmi {
+
+using namespace stratum;
+
+void add_path_elem(std::string elem_name, std::string elem_kv,
+                   ::gnmi::PathElem* elem) {
   elem->set_name(elem_name);
   if (!elem_kv.empty()) {
-    std::string key, value;
-    RE2::FullMatch(elem_kv, "\\[([^=]+)=([^\\]]+)\\]", &key, &value);
-    (*elem->mutable_key())[key] = value;
+    std::regex ex("\\[([^=]+)=([^\\]]+)\\]");
+    std::smatch sm;
+    std::regex_match(elem_kv, sm, ex);
+    (*elem->mutable_key())[sm.str(1)] = sm.str(2);
   }
 }
 
-void BuildGnmiPath(std::string path_str, ::gnmi::Path* path) {
-  re2::StringPiece input(path_str);
-  std::string elem_name, elem_kv;
-  while (RE2::Consume(&input, "/([^/\\[]+)(\\[([^=]+=[^\\]]+)\\])?", &elem_name,
-                      &elem_kv)) {
+void build_gnmi_path(std::string path_str, ::gnmi::Path* path) {
+  std::regex ex("/([^/\\[]+)(\\[([^=]+=[^\\]]+)\\])?");
+  std::sregex_iterator iter(path_str.begin(), path_str.end(), ex);
+  std::sregex_iterator end;
+  while (iter != end) {
+    std::smatch sm = *iter;
     auto* elem = path->add_elem();
-    AddPathElem(elem_name, elem_kv, elem);
+    add_path_elem(sm.str(1), sm.str(2), elem);
+    iter++;
   }
 }
 
-::gnmi::GetRequest BuildGnmiGetRequest(std::string path) {
+::gnmi::GetRequest build_gnmi_get_req(std::string path) {
   ::gnmi::GetRequest req;
-  BuildGnmiPath(path, req.add_path());
+  build_gnmi_path(path, req.add_path());
   req.set_encoding(::gnmi::PROTO);
   ::gnmi::GetRequest::DataType data_type;
   if (!::gnmi::GetRequest::DataType_Parse(FLAGS_get_type, &data_type)) {
@@ -157,57 +159,46 @@ void BuildGnmiPath(std::string path_str, ::gnmi::Path* path) {
   return req;
 }
 
-::gnmi::SetRequest BuildGnmiSetRequest(std::string path) {
+::gnmi::SetRequest build_gnmi_set_req(std::string path, std::string val) {
   ::gnmi::SetRequest req;
   ::gnmi::Update* update;
+  char *check;
+
   if (FLAGS_replace) {
     update = req.add_replace();
   } else {
     update = req.add_update();
   }
-  BuildGnmiPath(path, update->mutable_path());
-  if (!FLAGS_bool_val.empty()) {
-    update->mutable_val()->set_bool_val(StringToBool(FLAGS_bool_val));
-  } else if (!FLAGS_int_val.empty()) {
-    update->mutable_val()->set_int_val(stoll(FLAGS_int_val));
-  } else if (!FLAGS_uint_val.empty()) {
-    update->mutable_val()->set_uint_val(stoull(FLAGS_uint_val));
-  } else if (!FLAGS_float_val.empty()) {
-    update->mutable_val()->set_float_val(stof(FLAGS_float_val));
-  } else if (!FLAGS_proto_bytes.empty()) {
-    update->mutable_val()->set_proto_bytes(FLAGS_proto_bytes);
-  } else if (!FLAGS_string_val.empty()) {
-    update->mutable_val()->set_string_val(FLAGS_string_val);
-  } else if (!FLAGS_bytes_val_file.empty()) {
-    std::string buf;
-    ::stratum::ReadFileToString(FLAGS_bytes_val_file, &buf);
-    update->mutable_val()->set_bytes_val(buf);
+  build_gnmi_path(path, update->mutable_path());
+  strtol(val.c_str(), &check, 10);
+  if (*check) {
+    update->mutable_val()->set_string_val(val);
   } else {
-    std::cout << "No typed value set" << std::endl;
+    update->mutable_val()->set_int_val(stoull(val));
   }
   return req;
 }
 
-::gnmi::SetRequest BuildGnmiDeleteRequest(std::string path) {
+::gnmi::SetRequest build_gnmi_del_req(std::string path) {
   ::gnmi::SetRequest req;
   auto* del = req.add_delete_();
-  BuildGnmiPath(path, del);
+  build_gnmi_path(path, del);
   return req;
 }
 
-::gnmi::SubscribeRequest BuildGnmiSubOnchangeRequest(std::string path) {
+::gnmi::SubscribeRequest build_gnmi_sub_onchange_req(std::string path) {
   ::gnmi::SubscribeRequest sub_req;
   auto* sub_list = sub_req.mutable_subscribe();
   sub_list->set_mode(::gnmi::SubscriptionList::STREAM);
   sub_list->set_updates_only(true);
   auto* sub = sub_list->add_subscription();
   sub->set_mode(::gnmi::ON_CHANGE);
-  BuildGnmiPath(path, sub->mutable_path());
+  build_gnmi_path(path, sub->mutable_path());
   return sub_req;
 }
 
-::gnmi::SubscribeRequest BuildGnmiSubSampleRequest(std::string path,
-                                                   uint64 interval) {
+::gnmi::SubscribeRequest build_gnmi_sub_sample_req(
+    std::string path, ::google::protobuf::uint64 interval) {
   ::gnmi::SubscribeRequest sub_req;
   auto* sub_list = sub_req.mutable_subscribe();
   sub_list->set_mode(::gnmi::SubscriptionList::STREAM);
@@ -215,9 +206,72 @@ void BuildGnmiPath(std::string path_str, ::gnmi::Path* path) {
   auto* sub = sub_list->add_subscription();
   sub->set_mode(::gnmi::SAMPLE);
   sub->set_sample_interval(interval);
-  BuildGnmiPath(path, sub->mutable_path());
+  build_gnmi_path(path, sub->mutable_path());
   return sub_req;
 }
+
+bool extract_interface_node(char** path, char* node_path, bool* vhost_dev) {
+  char* key =  NULL;
+  char* value =  NULL;
+  int found_node = 0;
+
+  while (client_parse_key_value(path, &key, &value)) {
+    if (strcmp(key, FLAGS_device_key.c_str()) == 0) {
+      snprintf(node_path + strlen(node_path),
+               strlen(FLAGS_device_type_virtual_interface.c_str()) + 1, "%s",
+               FLAGS_device_type_virtual_interface.c_str());
+      // Validate the device value
+      if ((strcmp(value, "virtual-device") != 0) &&
+          (strcmp(value, "physical-device") != 0)) {
+        return -1;
+      }
+      if (strcmp(value, "virtual-device") == 0) {
+        std::cout << "setting vhost_dev = true.";
+        *vhost_dev = true;
+      }
+      found_node += 1;
+    }
+    if (strcmp(key, FLAGS_name_key.c_str()) == 0) {
+      // Hardcoded length of "[name=]/"
+      snprintf(node_path + strlen(node_path), strlen(value) + 9,
+               "[name=%s]/", value);
+      found_node += 1;
+    }
+    if (found_node == 2)
+      return 0;
+  }
+  return -1;
+}
+
+void traverse_params(char** path, char* node_path, char* config_value,
+                     bool& flag) {
+  char* key =  NULL;
+  char* value =  NULL;
+
+  if (client_parse_key_value(path, &key, &value)) {
+    if ((value != NULL) && value[0] != '\0') {
+      // This should be executed for a <key=value> pair, specifically for
+      // SET operation.
+      snprintf(node_path + strlen(node_path),
+               strlen(FLAGS_subtree_config.c_str()) + strlen(key) + 2,
+               "%s/%s", FLAGS_subtree_config.c_str(), key);
+      strcpy(config_value, value);
+      return;
+    } else if (key != NULL && key[0] != '\0') {
+      // This should be executed for a <key>, specifically for
+      // GET operation.
+      snprintf(node_path + strlen(node_path),
+               strlen(FLAGS_subtree_config.c_str()) + strlen(key) + 2,
+               "%s/%s", FLAGS_subtree_config.c_str(), key);
+      return;
+    }
+  }
+  flag = false;
+  return;
+}
+
+::grpc::ClientReaderWriterInterface<
+    ::gnmi::SubscribeRequest, ::gnmi::SubscribeResponse>* stream_reader_writer;
 
 ::util::Status Main(int argc, char** argv) {
 
@@ -234,6 +288,8 @@ void BuildGnmiPath(std::string path_str, ::gnmi::Path* path) {
   ::gflags::SetUsageMessage(kUsage);
   InitGoogle(argv[0], &argc, &argv, true);
   stratum::InitStratumLogging();
+  
+  bool vhost_device = false;
   if (argc < 2) {
     std::cout << kUsage << std::endl;
     return MAKE_ERROR(ERR_INVALID_PARAM) << "Invalid number of arguments.";
@@ -267,10 +323,11 @@ void BuildGnmiPath(std::string path_str, ::gnmi::Path* path) {
   auto channel = ::grpc::CreateChannel(
       FLAGS_grpc_addr,
       credentials_manager->GenerateExternalFacingClientCredentials());
-  auto stub = ::gnmi::gNMI::NewStub(channel);
   std::string cmd = std::string(argv[1]);
 
   if (cmd == "cap") {
+    auto stub = ::gnmi::gNMI::NewStub(channel);
+    ::grpc::ClientContext ctx;
     ::gnmi::CapabilityRequest req;
     PRINT_MSG(req, "REQUEST");
     ::gnmi::CapabilityResponse resp;
@@ -280,45 +337,85 @@ void BuildGnmiPath(std::string path_str, ::gnmi::Path* path) {
   }
 
   if (argc < 3) {
-    return MAKE_ERROR(ERR_INVALID_PARAM)
-           << "Missing path for " << cmd << " request.";
+    std::cout << "Missing path for " << cmd << " request\n";
+    return ::util::OkStatus();
   }
-  std::string path = std::string(argv[2]);
+
+  char *path = argv[2];
+  char buffer[MAX_STR_LENGTH];
+  bool params = true;
+
+  client_strzcpy(buffer, FLAGS_root_node.c_str(), MAX_STR_LENGTH);
+  if (extract_interface_node(&path, buffer, &vhost_device)) {
+    std::cout << "Invalid device and name information\n";
+    return ::util::OkStatus();
+  }
 
   if (cmd == "get") {
-    ::gnmi::GetRequest req = BuildGnmiGetRequest(path);
-    PRINT_MSG(req, "REQUEST");
+    auto stub = ::gnmi::gNMI::NewStub(channel);
+    ::grpc::ClientContext ctx;
+    char path1[MAX_STR_LENGTH] = {0};
+    char config_value[MAX_STR_LENGTH] = {0};
+
+    strcpy(path1, buffer);
+    traverse_params(&path, path1, config_value, params);
+    ::gnmi::GetRequest req = build_gnmi_get_req(path1);
     ::gnmi::GetResponse resp;
     RETURN_IF_GRPC_ERROR(stub->Get(&ctx, req, &resp));
-    PRINT_MSG(resp, "RESPONSE");
+    PRINT_MSG(resp, "Get Response from Server");
   } else if (cmd == "set") {
-    ::gnmi::SetRequest req = BuildGnmiSetRequest(path);
-    PRINT_MSG(req, "REQUEST");
-    ::gnmi::SetResponse resp;
-    RETURN_IF_GRPC_ERROR(stub->Set(&ctx, req, &resp));
-    PRINT_MSG(resp, "RESPONSE");
+    while(params) {
+      char path1[MAX_STR_LENGTH] = {0};
+      char config_value[MAX_STR_LENGTH] = {0};
+
+      strcpy(path1, buffer);
+      traverse_params(&path, path1, config_value, params);
+      // If device is 'virtual-device' and port type is 'link', consider it a
+      // 'vhost' type.
+      if (((strcmp(config_value, "link") == 0) ||
+           (strcmp(config_value, "LINK") == 0)) && vhost_device) {
+           strcpy(config_value, "vhost");
+      }
+      if (params) {
+        auto stub = ::gnmi::gNMI::NewStub(channel);
+        ::grpc::ClientContext ctx;
+        ::gnmi::SetRequest req = build_gnmi_set_req(path1, config_value);
+        ::gnmi::SetResponse resp;
+        RETURN_IF_GRPC_ERROR(stub->Set(&ctx, req, &resp));
+      }
+    }
+    std::cout << "Set request, successful...!!!" << std::endl;
   } else if (cmd == "del") {
-    ::gnmi::SetRequest req = BuildGnmiDeleteRequest(path);
-    PRINT_MSG(req, "REQUEST");
+    auto stub = ::gnmi::gNMI::NewStub(channel);
+    ::grpc::ClientContext ctx;
+    ::gnmi::SetRequest req = build_gnmi_del_req(path);
     ::gnmi::SetResponse resp;
     RETURN_IF_GRPC_ERROR(stub->Set(&ctx, req, &resp));
     PRINT_MSG(resp, "RESPONSE");
   } else if (cmd == "sub-onchange") {
-    auto stream_reader_writer = stub->Subscribe(&ctx);
-    ::gnmi::SubscribeRequest req = BuildGnmiSubOnchangeRequest(path);
-    PRINT_MSG(req, "REQUEST");
-    CHECK_RETURN_IF_FALSE(stream_reader_writer->Write(req)) << "Can not write request.";
+    auto stub = ::gnmi::gNMI::NewStub(channel);
+    ::grpc::ClientContext ctx;
+    auto stream_reader_writer_ptr = stub->Subscribe(&ctx);
+    stream_reader_writer = stream_reader_writer_ptr.get();
+    ::gnmi::SubscribeRequest req = build_gnmi_sub_onchange_req(path);
+    // CHECK_RETURN_IF_FALSE(stream_reader_writer->Write(req))
+    //     << "Cannot write request.";
+    stream_reader_writer->Write(req);
     ::gnmi::SubscribeResponse resp;
     while (stream_reader_writer->Read(&resp)) {
       PRINT_MSG(resp, "RESPONSE");
     }
     RETURN_IF_GRPC_ERROR(stream_reader_writer->Finish());
   } else if (cmd == "sub-sample") {
-    auto stream_reader_writer = stub->Subscribe(&ctx);
+    auto stub = ::gnmi::gNMI::NewStub(channel);
+    ::grpc::ClientContext ctx;
+    auto stream_reader_writer_ptr = stub->Subscribe(&ctx);
+    stream_reader_writer = stream_reader_writer_ptr.get();
     ::gnmi::SubscribeRequest req =
-        BuildGnmiSubSampleRequest(path, FLAGS_interval);
-    PRINT_MSG(req, "REQUEST");
-    CHECK_RETURN_IF_FALSE(stream_reader_writer->Write(req)) << "Can not write request.";
+        build_gnmi_sub_sample_req(path, FLAGS_interval);
+    // CHECK_RETURN_IF_FALSE(stream_reader_writer->Write(req))
+    //     << "Cannot write request.";
+    stream_reader_writer->Write(req);
     ::gnmi::SubscribeResponse resp;
     while (stream_reader_writer->Read(&resp)) {
       PRINT_MSG(resp, "RESPONSE");
@@ -327,14 +424,11 @@ void BuildGnmiPath(std::string path_str, ::gnmi::Path* path) {
   } else {
     return MAKE_ERROR(ERR_INVALID_PARAM) << "Unknown command: " << cmd;
   }
-  LOG(INFO) << "Done.";
 
   return ::util::OkStatus();
 }
 
-}  // namespace
 }  // namespace gnmi
-
 
 int main(int argc, char** argv) {
   return gnmi::Main(argc, argv).error_code();
