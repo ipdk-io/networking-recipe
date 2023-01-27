316ffd502ef1ba9cfebff8daa2bb6aa3d121bfb2
Derek G Foster <derek.foster@intel.com>
2023-01-26 16:57:19 -0800

    Fix issues in infrap4d command-line parsing
    - infrap4d_main parses the command line before it calls DpdkMain,
      which results in the dpdk-specific command-line flag defaults
      not being displayed by the infrap4d --help command.
    
      Addressed this issue by having infrap4d_main call
      stratum::hal::tdi::InitCommandLineFlags() before it calls
      gflags::ParseCommandLineFlags().
    
    - Updated stratum submodule pointer.
    
    Signed-off-by: Derek G Foster <derek.foster@intel.com>

--- infrap4d/infrap4d_main.cc
+++ infrap4d/infrap4d_main.cc
@@ -19,17 +19,20 @@ extern "C"  {
 DEFINE_bool(detach, true, "Run infrap4d in attached mode");
 
 // Invokes the main function for the TDI target.
-static inline ::util::Status target_main(
-    int argc, char** argv, absl::Notification * ready_sync,
-    absl::Notification* done_sync) {
+static inline ::util::Status target_main(absl::Notification* ready_sync,
+                                         absl::Notification* done_sync) {
 #if defined(DPDK_TARGET)
-  return stratum::hal::tdi::DpdkMain(argc, argv, ready_sync, done_sync);
+  return stratum::hal::tdi::DpdkMain(ready_sync, done_sync);
 #else
   #error "TDI target type not defined!"
 #endif
 }
 
 int main(int argc, char* argv[]) {
+  // Set target-specific defaults
+  stratum::hal::tdi::InitCommandLineFlags();
+
+  // Parse infrap4d command line
   gflags::ParseCommandLineFlags(&argc, &argv, false);
 
   if (FLAGS_detach) {
@@ -43,9 +46,9 @@ int main(int argc, char* argv[]) {
   krnlmon_create_main_thread(&ready_sync);
   krnlmon_create_shutdown_thread(&done_sync);
 
-  auto status = target_main(argc, argv, &ready_sync, &done_sync);
+  auto status = target_main(&ready_sync, &done_sync);
   if (!status.ok()) {
-     //TODO: Figure out logging for infrap4d
+     // TODO: Figure out logging for infrap4d
      return status.error_code();
    }
 
