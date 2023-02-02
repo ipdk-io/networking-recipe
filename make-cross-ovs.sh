d945b5f321343f1cc57ba562e8770dbf6fc83071
Derek G Foster <derek.foster@intel.com>
2023-02-02 01:34:04 +0000

    Support cross-compilation of OvS
    - Modified build script to support cross-compilation of OVS.
    
    - Supplied a sample script to demonstrate how to cross-compile
      OVS for the ES2K ACC platform.
    
    Signed-off-by: Derek G Foster <derek.foster@intel.com>

---
+++ make-cross-ovs.sh
@@ -0,0 +1,11 @@
+_BUILD_DIR=ovs/build
+_INSTALL_DIR=${SDKTARGETSYSROOT}/opt/ovs
+
+rm -fr ${_BUILD_DIR} ${_INSTALL_DIR}
+
+cmake -S ovs -B ${_BUILD_DIR} \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -DCMAKE_INSTALL_PREFIX=${_INSTALL_DIR} \
+    -DP4OVS=ON -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+
+cmake --build ${_BUILD_DIR} -j8 -- V=0    
