8a228f63d122f8c9ef7dc559bed33bbf227a9a62
Derek G Foster <derek.foster@intel.com>
2023-02-03 21:44:51 +0000

    Update example dependency build scripts
    - Modified to sample dependency build scripts to check whether the
      cross-compilation environment variables are defined and issue an
      appropriate error message if they are in the wrong state.
    
    - Added shebangs to the scripts.
    
    Signed-off-by: Derek G Foster <derek.foster@intel.com>

--- setup/make-cross-deps.sh
+++ setup/make-cross-deps.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+
 # Sample script to configure and build the dependency libraries
 # on the development host when cross-compiling for the ES2K ACC.
+
+if [ -z "${SDKTARGETSYSROOT}" ]; then
+    echo ""
+    echo "-----------------------------------------------------"
+    echo "Error: SDKTARGETSYSROOT is not defined!"
+    echo "Did you forget to source the environment variables?"
+    echo "-----------------------------------------------------"
+    echo ""
+    exit 1
+fi
 
 CROSS_BUILD=build
 CROSS_INSTALL=${SDKTARGETSYSROOT}/opt/deps
--- setup/make-host-deps.sh
+++ setup/make-host-deps.sh
@@ -1,12 +1,24 @@
+#!/bin/bash
+
 # Sample script to configure and build the subset of the dependency
 # libraries needed to compile the Protobuf files on the development
 # host when cross-compiling for the ES2K ACC platform.
 
-# The Protobuf compiler and grpc plubins run natively on the development
+# The Protobuf compiler and grpc plugins run natively on the development
 # host. They generate C++ source and header files that can be compiled
 # to run natively on the host or cross-compiled to run on the target
 # platform.
 
+if [ -n "${SDKTARGETSYSROOT}" ]; then
+    echo ""
+    echo "-----------------------------------------------------"
+    echo "Error: SDKTARGETSYSROOT is defined!"
+    echo "The host dependencies must be built WITHOUT sourcing"
+    echo "the cross-compile environment variables."
+    echo "-----------------------------------------------------"
+    echo 
+    exit 1
+fi
 HOST_BUILD=build
 HOST_INSTALL=host-deps
 
