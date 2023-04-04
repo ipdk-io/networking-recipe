88d8bc5242d2ffbdec3ba0e887ec38645f0249d0
Sandeep Nagapattinam <sandeep.nagapattinam@intel.com>
2023-04-05 04:54:24 +0530

    Fix OVS hang issue for Linux networking usecase (#71)
    OVS spawns revalidator threads and handler threads for datapath,
    when MAC is learnt at the same time by OVS, procesing of these learnt MAC's
    are taken by two different threads.
    In LNW use case, election_id is decided by TimeBasedElectionId() API.
    When both the threads start processing learnt MAC at the same time, we
    observed that both the threads are getting same election_id. This causes
    one of the threads to hang and doesnt process any other notifications
    for datapath.
    To avoid collision of election ID, we have added thread ID as well to
    get a unique election ID at any given time.
    
    Signed-off-by: Sandeep N <sandeep.nagapattinam@intel.com>

--- ovs-p4rt/ovs_p4rt_session.cc
+++ ovs-p4rt/ovs_p4rt_session.cc
@@ -86,6 +86,20 @@ absl::StatusOr<std::unique_ptr<OvsP4rtSession>> OvsP4rtSession::Create(
     const std::string& address,
     const std::shared_ptr<grpc::ChannelCredentials>& credentials,
     uint32_t device_id, absl::uint128 election_id) {
+
+  /* OVS spawns multiple revalidator threads and handler threads to handle
+   * datapath Notifications.
+   * When multiple L2 MAC's are learnt at the same time, the learn information
+   * can come from revlidator thread or handler thread. On a worst case
+   * scenario, both the threads will call OvsP4rtSession::Create exactly at
+   * the same time and both threads can get same election_id based on API
+   * TimeBasedElectionId(). This causes one of the threads to get stuck and
+   * no further data path information is received by the thread.
+   * To avoid this issue, we add Thread ID to the election ID to generate an
+   * unique election_id, even when two threads receive MAC learn at the same
+   * time.
+   */
+  election_id = election_id + (absl::uint128)pthread_self();
   return Create(CreateP4RuntimeStub(address, credentials), device_id,
                 election_id);
 }
