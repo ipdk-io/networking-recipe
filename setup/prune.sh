7d967321abb987eaf4f9ff8d1e1ae1e3fd0a5df6
Derek G Foster <derek.foster@intel.com>
2023-06-19 18:09:35 -0700

    Implement cmake Find Module for OVS
    - Reimplement the OVS library interface logic as a cmake Find Module.
      In keeping with modern practice, define a target for each library.
    
    - Change default value of the OVS_INSTALL_DIR variable from
      ${CMAKE_INSTALL_PREFIX} to $ENV{OVS_INSTALL}. The OVS install tree
      is an input to the P4 Control Plane build, not an output.
    
    - Delete the OVS_SOURCE_DIR variable. The P4 Control Plane build
      uses the OVS install tree, not its source tree.
    
    - Fix a couple of issues in the setup/prune.sh script.
    
    - Add a .clang-format format to the superproject.
    
    Signed-off-by: Derek G Foster <derek.foster@intel.com>

--- setup/prune.sh
+++ setup/prune.sh
@@ -3,14 +3,18 @@
 # Copyright 2023 Intel Corporation
 # SPDX-License-Identifier: Apache 2.0
 #
-# Removes duplicate copies of third-party packages.
+# Removes unnecessary third-party packages.
 #
 
+# duplicate packages
 rm -fr grpc/third_party/abseil-cpp
 rm -fr grpc/third_party/bloaty/abseil-cpp
+rm -fr grpc/third_party/bloaty/third_party/abseil-cpp
 rm -fr grpc/third_party/bloaty/third_party/protobuf
 rm -fr grpc/third_party/bloaty/third_party/zlib
-rm -fr grpc/third_party/boring-ssl-with-bazel
 rm -fr grpc/third_party/cares
 rm -fr grpc/third_party/protobuf
 rm -fr grpc/third_party/zlib
+
+# unused packages
+rm -fr grpc/third_party/boringssl-with-bazel
