b1332b659ef586956b57b75b346f08ba60ba9b6a
Derek G Foster <derek.foster@intel.com>
2023-07-05 13:00:00 -0700

    Use find_package() to locate unwind module (#190)
    - Use find_package() instead of find_library() to find Unwind library.
    
    - Resolve conflicts between unwind and unbound updates.
    
    Signed-off-by: Derek G Foster <derek.foster@intel.com>

--- cmake/FindUnwind.cmake
+++ cmake/FindUnwind.cmake
@@ -1,4 +1,4 @@
-# FindUnbound.cmake - Find libunbound package.
+# FindUnwind.cmake - Find libunwind package.
 #
 # Copyright 2023 Intel Corporation
 # SPDX-License-Identifier: Apache 2.0
@@ -9,51 +9,51 @@
 #-----------------------------------------------------------------------
 find_package(PkgConfig)
 
-pkg_check_modules(UNBOUND libunbound)
-if(NOT UNBOUND_FOUND)
+pkg_check_modules(UNWIND libunwind)
+if(NOT UNWIND_FOUND)
     return()
 endif()
 
 #-----------------------------------------------------------------------
 # Find libraries and include directories.
 #-----------------------------------------------------------------------
-find_path(UNBOUND_INCLUDE_DIR
-    NAMES "unbound.h"
-    PATHS ${UNBOUND_INCLUDE_DIRS}
+find_path(UNWIND_INCLUDE_DIR
+    NAMES "unwind.h"
+    PATHS ${UNWIND_INCLUDE_DIRS}
 )
 
-find_library(UNBOUND_LIBRARY
-    NAMES unbound
-    PATHS ${UNBOUND_LIBRARY_DIRS}
+find_library(UNWIND_LIBRARY
+    NAMES unwind
+    PATHS ${UNWIND_LIBRARY_DIRS}
 )
 
 #-----------------------------------------------------------------------
 # Get version number
 #-----------------------------------------------------------------------
-if(UNBOUND_VERSION)
-    set(UNBOUND_VERSION ${UNBOUND_VERSION} CACHE STRING "libunbound version")
+if(UNWIND_VERSION)
+    set(UNWIND_VERSION ${UNWIND_VERSION} CACHE STRING "libunwind version")
 endif()
 
 #-----------------------------------------------------------------------
 # Handle REQUIRED and QUIET arguments
 #-----------------------------------------------------------------------
-find_package_handle_standard_args(UNBOUND
+find_package_handle_standard_args(UNWIND
     REQUIRED_VARS
-        UNBOUND_LIBRARY
-        UNBOUND_INCLUDE_DIR
+        UNWIND_LIBRARY
+        UNWIND_INCLUDE_DIR
     VERSION_VAR
-        UNBOUND_VERSION
+        UNWIND_VERSION
 )
 
-mark_as_advanced(UNBOUND_INCLUDE_DIR UNBOUND_LIBRARY UNBOUND_VERSION)
+mark_as_advanced(UNWIND_INCLUDE_DIR UNWIND_LIBRARY UNWIND_VERSION)
 
 #-----------------------------------------------------------------------
 # Define library target
 #-----------------------------------------------------------------------
-add_library(unbound UNKNOWN IMPORTED)
+add_library(unwind UNKNOWN IMPORTED)
 
-set_target_properties(unbound PROPERTIES
-    IMPORTED_LOCATION ${UNBOUND_LIBRARY}
-    INTERFACE_INCLUDE_DIRECTORIES ${UNBOUND_INCLUDE_DIR}
+set_target_properties(unwind PROPERTIES
+    IMPORTED_LOCATION ${UNWIND_LIBRARY}
+    INTERFACE_INCLUDE_DIRECTORIES ${UNWIND_INCLUDE_DIR}
     IMPORTED_LINK_INTERFACE_LANGUAGES "C"
 )
