fbc0956dd362601c9cf293c9ed784dc5cd158920
Derek G Foster <derek.foster@intel.com>
2023-08-12 13:00:36 -0700

    Refine config-cross-recipe script
    - Break command-line help into Paths and Options sections.
    
    - Move dry-run display logic to print_cmake_params().
    
    - Replace ES2K_TARGET=YES with TDI_TARGET=ES2K.
    
    - Remove unnecessary echo statements.
    
    - Adopt recent conventions from make-all.sh.
    
    - Address shellcheck issues
    
    - Replace old script in top-level directory with symlink to
      revised script in scripts/es2k.
    
    Signed-off-by: Derek G Foster <derek.foster@intel.com>

--- scripts/es2k/config-cross-recipe.sh
+++ scripts/es2k/config-cross-recipe.sh
@@ -20,34 +20,43 @@ fi
 
 _SYSROOT=${SDKTARGETSYSROOT}
 
-# Default values
-_BLD_DIR=build
+##################
+# Default values #
+##################
+
 _BLD_TYPE=RelWithDebInfo
 _DEPS_DIR="${DEPEND_INSTALL:-//opt/deps}"
-_DRY_RUN=false
 _HOST_DIR="${HOST_INSTALL:-setup/host-deps}"
 _OVS_DIR="${OVS_INSTALL:-//opt/ovs}"
 _PREFIX=install
 _SDE_DIR="${SDE_INSTALL:-//opt/p4sde}"
 _TOOLFILE=${CMAKE_TOOLCHAIN_FILE}
 
-# Displays help text
+_BLD_DIR=build
+_DRY_RUN=0
+
+##############
+# print_help #
+##############
+
 print_help() {
     echo ""
-    echo "Configure recipe build"
+    echo "Configure P4 Control Plane build"
     echo ""
-    echo "Options:"
+    echo "Paths:"
     echo "  --build=DIR      -B  Build directory path [${_BLD_DIR}]"
     echo "  --deps=DIR*      -D  Target dependencies directory [${_DEPS_DIR}]"
-    echo "  --dry-run        -n  Display cmake parameter values and exit"
-    echo "  --help           -h  Display this help text"
-    echo "  --hostdeps=DIR*  -H  Host dependencies directory [${_HOST_DIR}]"
+    echo "  --host=DIR*      -H  Host dependencies directory [${_HOST_DIR}]"
     echo "  --ovs=DIR*       -O  OVS install directory [${_OVS_DIR}]"
-    echo "  --no-krnlmon         Exclude Kernel Monitor"
-    echo "  --no-ovs             Exclude OVS support"
     echo "  --prefix=DIR*    -P  Install directory prefix [${_PREFIX}]"
     echo "  --sde=DIR*       -S  SDE install directory [${_SDE_DIR}]"
     echo "  --toolchain=FILE -T  CMake toolchain file"
+    echo ""
+    echo "Options:"
+    echo "  --dry-run        -n  Display cmake parameter values and exit"
+    echo "  --help           -h  Display this help text"
+    echo "  --no-krnlmon         Exclude Kernel Monitor"
+    echo "  --no-ovs             Exclude OVS support"
     echo ""
     echo "* '//' at the beginning of the directory path will be replaced"
     echo "  with the sysroot directory path."
@@ -62,42 +71,54 @@ print_help() {
     echo ""
 }
 
-# Parse options
-SHORTOPTS=B:D:H:O:P:S:T:hn
-LONGOPTS=build:,deps:,dry-run,help,hostdeps:,ovs:,prefix:,sde:,toolchain:
-LONGOPTS=${LONGOPTS},no-krnlmon,no-ovs,rfs-enable
+######################
+# print_cmake_params #
+######################
 
-eval set -- `getopt -o ${SHORTOPTS} --long ${LONGOPTS} -- "$@"`
+print_cmake_params() {
+    echo ""
+    echo "CMAKE_BUILD_TYPE=${_BLD_TYPE}"
+    echo "CMAKE_INSTALL_PREFIX=${_PREFIX}"
+    echo "CMAKE_TOOLCHAIN_FILE=${_TOOLFILE}"
+    echo "DEPEND_INSTALL_DIR=${_DEPS_DIR}"
+    echo "HOST_DEPEND_DIR=${_HOST_DIR}"
+    echo "OVS_INSTALL_DIR=${_OVS_DIR}"
+    echo "SDE_INSTALL_DIR=${_SDE_DIR}"
+    [ -n "${_WITH_KRNLMON}" ] && echo "${_WITH_KRNLMON:2}"
+    [ -n "${_WITH_OVSP4RT}" ] && echo "${_WITH_OVSP4RT:2}"
+    echo ""
+}
 
+######################
+# Parse command line #
+######################
+
+SHORTOPTS=B:D:H:O:P:S:T:
+SHORTOPTS=${SHORTOPTS}hn
+
+LONGOPTS=build:,deps:,hostdeps:,ovs:,prefix:,sde:,toolchain:
+LONGOPTS=${LONGOPTS},dry-run,help,no-krnlmon,no-ovs
+
+GETOPTS=$(getopt -o ${SHORTOPTS} --long ${LONGOPTS} -- "$@")
+eval set -- "${GETOPTS}"
+
+# Process command-line options.
 while true ; do
     case "$1" in
+    # Paths
     -B|--build)
-        echo "Build directory: $2"
         _BLD_DIR=$2
         shift 2 ;;
     -D|--deps)
         _DEPS_DIR=$2
         shift 2 ;;
-    -n|--dry-run)
-        _DRY_RUN=true
-        shift 1 ;;
-    -h|--help)
-        print_help
-        exit 99 ;;
     -H|--hostdeps)
         _HOST_DIR=$2
         shift 2 ;;
-    --no-krnlmon)
-        _WITH_KRNLMON=FALSE
-        shift 1 ;;
-    --no-ovs)
-        _WITH_OVSP4RT=FALSE
-        shift 1 ;;
     -O|--ovs)
         _OVS_DIR=$2
         shift 2 ;;
     -P|--prefix)
-        echo "Install prefix: $2"
         _PREFIX=$2
         shift 2 ;;
     -S|--sde)
@@ -106,6 +127,19 @@ while true ; do
     -T|--toolchain)
         _TOOLFILE=$2
         shift 2 ;;
+    # Options
+    -n|--dry-run)
+        _DRY_RUN=1
+        shift 1 ;;
+    -h|--help)
+        print_help
+        exit 99 ;;
+    --no-krnlmon)
+        _WITH_KRNLMON=FALSE
+        shift 1 ;;
+    --no-ovs)
+        _WITH_OVSP4RT=FALSE
+        shift 1 ;;
     --)
         shift
         break ;;
@@ -114,6 +148,10 @@ while true ; do
         exit 1 ;;
     esac
 done
+
+######################
+# Process parameters #
+######################
 
 # Substitute ${_SYSROOT}/ for // prefix
 [ "${_DEPS_DIR:0:2}" = "//" ] && _DEPS_DIR=${_SYSROOT}/${_DEPS_DIR:2}
@@ -126,31 +164,27 @@ done
 [ -n "${_WITH_KRNLMON}" ] && _WITH_KRNLMON=-DWITH_KRNLMON=${_WITH_KRNLMON}
 [ -n "${_WITH_OVSP4RT}" ] && _WITH_OVSP4RT=-DWITH_OVSP4RT=${_WITH_OVSP4RT}
 
-if [ "${_DRY_RUN}" = "true" ]; then
-    echo ""
-    echo "CMAKE_BUILD_TYPE=${_BLD_TYPE}"
-    echo "CMAKE_INSTALL_PREFIX=${_PREFIX}"
-    echo "CMAKE_TOOLCHAIN_FILE=${_TOOLFILE}"
-    echo "DEPEND_INSTALL_DIR=${_DEPS_DIR}"
-    echo "HOST_DEPEND_DIR=${_HOST_DIR}"
-    echo "OVS_INSTALL_DIR=${_OVS_DIR}"
-    echo "SDE_INSTALL_DIR=${_SDE_DIR}"
-    [ -n "${_WITH_KRNLMON}" ] && echo "${_WITH_KRNLMON:2}"
-    [ -n "${_WITH_OVSP4RT}" ] && echo "${_WITH_OVSP4RT:2}"
-    echo ""
+# Show parameters if this is a dry run
+if [ ${_DRY_RUN} -ne 0 ]; then
+    print_cmake_params
     exit 0
 fi
 
-rm -fr ${_BLD_DIR}
+#######################
+# Configure the build #
+#######################
 
-cmake -S . -B ${_BLD_DIR} \
+rm -fr "${_BLD_DIR}"
+
+cmake -S . -B "${_BLD_DIR}" \
     -DCMAKE_BUILD_TYPE=${_BLD_TYPE} \
-    -DCMAKE_INSTALL_PREFIX=${_PREFIX} \
-    -DCMAKE_TOOLCHAIN_FILE=${_TOOLFILE} \
-    -DDEPEND_INSTALL_DIR=${_DEPS_DIR} \
-    -DHOST_DEPEND_DIR=${_HOST_DIR} \
-    -DOVS_INSTALL_DIR=${_OVS_DIR} \
-    -DSDE_INSTALL_DIR=${_SDE_DIR} \
-    ${_WITH_KRNLMON} ${_WITH_OVSP4RT} \
+    -DCMAKE_INSTALL_PREFIX="${_PREFIX}" \
+    -DCMAKE_TOOLCHAIN_FILE="${_TOOLFILE}" \
+    -DDEPEND_INSTALL_DIR="${_DEPS_DIR}" \
+    -DHOST_DEPEND_DIR="${_HOST_DIR}" \
+    -DOVS_INSTALL_DIR="${_OVS_DIR}" \
+    -DSDE_INSTALL_DIR="${_SDE_DIR}" \
+    "${_WITH_KRNLMON}" \
+    "${_WITH_OVSP4RT}" \
     -DSET_RPATH=TRUE \
-    -DES2K_TARGET=ON
+    -DTDI_TARGET=ES2K
