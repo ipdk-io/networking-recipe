24df9ad5834630e02886dd25ffb5dc68deb44f3f
Derek G Foster <derek.foster@intel.com>
2023-08-16 06:00:43 -0700

    Daemonize infrap4d without krnlmon (#250)
    - In the original implementation, infrap4d without krnlmon consisted
      of nothing but Stratum. There was no support for running as a
      daemon.
    
      This CL implements a lightweight version of infrap4d_main.cc
      (infrap4d_lite.cc) that includes support for daemonization.
    
    Signed-off-by: Derek G Foster <derek.foster@intel.com>

---
+++ infrap4d/infrap4d_lite.cc
@@ -0,0 +1,32 @@
+// infrap4d_main without krnlmon
+
+// Copyright 2022-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#include "gflags/gflags.h"
+#include "stratum/glue/status/status.h"
+#include "stratum/hal/bin/tdi/main.h"
+
+extern "C"  {
+#include "daemon/daemon.h"
+}
+
+DEFINE_bool(detach, true, "Run infrap4d in detached mode");
+
+int main(int argc, char* argv[]) {
+  // Parse infrap4d command line
+  stratum::hal::tdi::ParseCommandLine(argc, argv, true);
+
+  if (FLAGS_detach) {
+      daemonize_start(false);
+      daemonize_complete();
+  }
+
+  auto status = stratum::hal::tdi::Main();
+  if (!status.ok()) {
+     // TODO: Figure out logging for infrap4d
+     return status.error_code();
+   }
+
+  return 0;
+}
