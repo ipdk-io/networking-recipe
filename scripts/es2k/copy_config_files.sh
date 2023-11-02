18775d91a99e871cde8271d46652f2b8509452e2
Satish Pitchikala <satish.pitchikala@intel.com>
2023-11-02 21:42:41 +0530

    Missed to install ipsec related config files
    Signed-off-by: Satish Pitchikala <satish.pitchikala@intel.com>

--- scripts/es2k/copy_config_files.sh
+++ scripts/es2k/copy_config_files.sh
@@ -18,17 +18,32 @@ then
     return 0
 fi
 
-export IPDK_INSTALL=$1
+export P4CP_INSTALL=$1
 export SDE_INSTALL=$2
 
+SOURCE_DIR=${P4CP_INSTALL}/share/stratum/es2k
+TARGET_DIR=/usr/share/stratum/es2k
+TLS_CERTS_DIR=/usr/share/stratum
+
 #... Create required directories and copy the config files ...#
-cd $IPDK_INSTALL
+cd $P4CP_INSTALL
 sudo mkdir -p /etc/stratum/
 sudo mkdir -p /var/log/stratum/
 sudo mkdir -p /usr/share/stratum/es2k
+sudo mkdir -p /usr/share/target_sys/
 sudo mkdir -p /usr/share/bf_rt_shared
-sudo cp $IPDK_INSTALL/share/stratum/es2k/es2k_port_config.pb.txt /usr/share/stratum/es2k/
-sudo cp $IPDK_INSTALL/share/stratum/es2k/es2k_skip_p4.conf /usr/share/stratum/es2k/
-sudo cp $SDE_INSTALL/share/bf_rt_shared/tdi_pktio.json /usr/share/bf_rt_shared/
+
+sudo cp ${SOURCE_DIR}/es2k_port_config.pb.txt /usr/share/stratum/es2k/
+sudo cp ${SOURCE_DIR}/es2k_skip_p4.conf /usr/share/stratum/es2k/
+
+#... Install files required for TLS certificate generation ...#
+sudo cp ${SOURCE_DIR}/ca.conf ${TLS_CERTS_DIR}/
+sudo cp ${SOURCE_DIR}/grpc-client.conf ${TLS_CERTS_DIR}/
+sudo cp ${P4CP_INSTALL}/sbin/generate-certs.sh ${TLS_CERTS_DIR}/
+
 sudo cp $SDE_INSTALL/share/target_sys/zlog-cfg /usr/share/target_sys/
+
+#... Install packetIO json file ...#
+sudo cp $SDE_INSTALL/share/bf_rt_shared/tdi_pktio.json /usr/share/bf_rt_shared/
+
 set +e
