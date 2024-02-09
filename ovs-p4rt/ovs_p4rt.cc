422b3c3169f4b36ee0d702eaaf512777aef5753f
Sabeel Ansari <35787514+5abeel@users.noreply.github.com>
2024-02-09 12:54:09 -0600

    RBAC client side support (#402)
    * RBAC client side support
    
    Signed-off-by: Sabeel Ansari <sabeel.ansari@intel.com>
    
    * Edit CMakeLists for ovs-p4rt
    
    Signed-off-by: Sabeel Ansari <sabeel.ansari@intel.com>
    
    * Updae CMakeLists to add stratum_static target link to ovs_sidecar_o
    
    Signed-off-by: Sabeel Ansari <sabeel.ansari@intel.com>
    
    * Update role config file name based on comment
    
    Signed-off-by: Sabeel Ansari <sabeel.ansari@intel.com>
    
    * Update comment to retrigger CI check
    
    Signed-off-by: Sabeel Ansari <sabeel.ansari@intel.com>
    
    * Update ovs submodule
    
    Signed-off-by: Sabeel Ansari <sabeel.ansari@intel.com>
    
    ---------
    
    Signed-off-by: Sabeel Ansari <sabeel.ansari@intel.com>

--- ovs-p4rt/ovs_p4rt.cc
+++ ovs-p4rt/ovs_p4rt.cc
@@ -1,4 +1,4 @@
-// Copyright 2022-2023 Intel Corporation
+// Copyright 2022-2024 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 // TODO: ovs-p4rt logging
 
@@ -15,9 +15,13 @@
 #include "es2k/p4_name_mapping.h"
 #endif
 
+#define DEFAULT_OVS_P4RT_ROLE_NAME "ovs-p4rt"
+
 ABSL_FLAG(std::string, grpc_addr, "localhost:9559",
           "P4Runtime server address.");
 ABSL_FLAG(uint64_t, device_id, 1, "P4Runtime device ID.");
+ABSL_FLAG(std::string, role_name, DEFAULT_OVS_P4RT_ROLE_NAME,
+          "P4 config role name.");
 
 namespace ovs_p4rt {
 
@@ -2022,7 +2026,7 @@ void ConfigFdbTableEntry(struct mac_learning_info learn_info,
   // Start a new client session.
   auto status_or_session = ovs_p4rt::OvsP4rtSession::Create(
       absl::GetFlag(FLAGS_grpc_addr), GenerateClientCredentials(),
-      absl::GetFlag(FLAGS_device_id));
+      absl::GetFlag(FLAGS_device_id), absl::GetFlag(FLAGS_role_name));
   if (!status_or_session.ok()) return;
 
   // Unwrap the session from the StatusOr object.
@@ -2158,7 +2162,7 @@ void ConfigRxTunnelSrcTableEntry(struct tunnel_info tunnel_info,
   // Start a new client session.
   auto status_or_session = OvsP4rtSession::Create(
       absl::GetFlag(FLAGS_grpc_addr), GenerateClientCredentials(),
-      absl::GetFlag(FLAGS_device_id));
+      absl::GetFlag(FLAGS_device_id), absl::GetFlag(FLAGS_role_name));
   if (!status_or_session.ok()) return;
 
   // Unwrap the session from the StatusOr object.
@@ -2185,7 +2189,7 @@ void ConfigTunnelSrcPortTableEntry(struct src_port_info tnl_sp,
   // Start a new client session.
   auto status_or_session = OvsP4rtSession::Create(
       absl::GetFlag(FLAGS_grpc_addr), GenerateClientCredentials(),
-      absl::GetFlag(FLAGS_device_id));
+      absl::GetFlag(FLAGS_device_id), absl::GetFlag(FLAGS_role_name));
   if (!status_or_session.ok()) return;
 
   // Unwrap the session from the StatusOr object.
@@ -2220,7 +2224,7 @@ void ConfigSrcPortTableEntry(struct src_port_info vsi_sp, bool insert_entry) {
   // Start a new client session.
   auto status_or_session = OvsP4rtSession::Create(
       absl::GetFlag(FLAGS_grpc_addr), GenerateClientCredentials(),
-      absl::GetFlag(FLAGS_device_id));
+      absl::GetFlag(FLAGS_device_id), absl::GetFlag(FLAGS_role_name));
   if (!status_or_session.ok()) return;
 
   // Unwrap the session from the StatusOr object.
@@ -2279,7 +2283,7 @@ void ConfigVlanTableEntry(uint16_t vlan_id, bool insert_entry) {
   // Start a new client session.
   auto status_or_session = OvsP4rtSession::Create(
       absl::GetFlag(FLAGS_grpc_addr), GenerateClientCredentials(),
-      absl::GetFlag(FLAGS_device_id));
+      absl::GetFlag(FLAGS_device_id), absl::GetFlag(FLAGS_role_name));
   if (!status_or_session.ok()) return;
 
   // Unwrap the session from the StatusOr object.
@@ -2309,7 +2313,7 @@ void ConfigFdbTableEntry(struct mac_learning_info learn_info,
   // Start a new client session.
   auto status_or_session = ovs_p4rt::OvsP4rtSession::Create(
       absl::GetFlag(FLAGS_grpc_addr), GenerateClientCredentials(),
-      absl::GetFlag(FLAGS_device_id));
+      absl::GetFlag(FLAGS_device_id), absl::GetFlag(FLAGS_role_name));
   if (!status_or_session.ok()) return;
 
   // Unwrap the session from the StatusOr object.
@@ -2363,7 +2367,7 @@ void ConfigTunnelTableEntry(struct tunnel_info tunnel_info, bool insert_entry) {
   // Start a new client session.
   auto status_or_session = OvsP4rtSession::Create(
       absl::GetFlag(FLAGS_grpc_addr), GenerateClientCredentials(),
-      absl::GetFlag(FLAGS_device_id));
+      absl::GetFlag(FLAGS_device_id), absl::GetFlag(FLAGS_role_name));
   if (!status_or_session.ok()) return;
 
   // Unwrap the session from the StatusOr object.
