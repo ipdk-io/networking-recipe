08e6c72fe2c0d7031b6f4eb86a555cb5a854e3cc
Derek G Foster <derek.foster@intel.com>
2024-04-21 16:37:34 -0700

    Simplify generation of ovs build artifacts
    - Defined set_ovs_target_properties() function to set properties
      that are common to both artifacts.
    
    Signed-off-by: Derek G Foster <derek.foster@intel.com>

--- ovs-p4rt/ovs/testcontroller.cmake
+++ ovs-p4rt/ovs/testcontroller.cmake
@@ -24,31 +24,6 @@ target_link_libraries(ovs-testcontroller
         rt
 )
 
-if(TARGET ovs::avx512)
-    target_link_libraries(ovs-testcontroller PUBLIC ovs::avx512)
-endif()
-
-if(TARGET unbound)
-    target_link_libraries(ovs-testcontroller PUBLIC unbound)
-endif()
-
-if(TARGET unwind::unwind)
-    target_link_libraries(ovs-testcontroller PUBLIC unwind::unwind)
-endif()
-
-target_link_libraries(ovs-testcontroller PUBLIC
-    absl::strings
-    absl::statusor
-    absl::flags_private_handle_accessor
-    absl::flags
-)
-
-target_link_libraries(ovs-testcontroller PUBLIC stratum_static)
-
-target_link_libraries(ovs-testcontroller PUBLIC
-    p4_role_config_proto
-    p4runtime_proto
-    stratum_proto
-)
+set_ovs_target_properties(ovs-testcontroller)
 
 install(TARGETS ovs-testcontroller DESTINATION bin)
