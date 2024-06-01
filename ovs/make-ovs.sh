f7ed748886ddc1a3f3cfa09f529c8adb6da0a917
Derek Foster <derek.foster@intel.com>
2024-05-31 17:46:02 -0700

    Address a couple of issues in make-ovs.sh
    - Changed file mode to executable.
    - Added a shebang line.
    - Updated OVS_INSTALL_PREFIX to CMAKE_INSTALL_PREFIX.
    
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- ovs/make-ovs.sh
+++ ovs/make-ovs.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 rm -fr build install
 
 # ${_OVS_BLD} ${_OVS_DIR} ${_TOOLCHAIN_FILE}
-cmake -S . -B build -DOVS_INSTALL_PREFIX=install -DP4OVS=ON
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX=install -DP4OVS=ON
 cmake --build build -j6 -- V=0
