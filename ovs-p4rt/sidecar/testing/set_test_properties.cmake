4c3740a611859aeef4fed25944d9725e0622eb40
Derek G Foster <derek.foster@intel.com>
2024-06-25 13:03:38 -0700

    Add support for OVSP4RT unit testing (#509)
    - Created infrastructure for ovs-p4rt unit testing.
    
      - Implemented a `stratum_utils` library, which provides the
        facilities used by ovs-p4rt. This makes it possible to build
        ovs-p4rt without building all of Stratum.
    
      - Created a header file (`ovsp4rt_private.h`) to expose
        prototypes for the functions under test.
    
    - Developed unit test for the `PrepareL2ToTunnelV4()` and
      `PrepareL2ToTunnelV6()` functions.
    
    - Eliminated unnecessary cmake dependencies.

---
+++ ovs-p4rt/sidecar/testing/set_test_properties.cmake
@@ -0,0 +1,37 @@
+#
+# Copyright 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+#
+
+#-----------------------------------------------------------------------
+# set_test_properties()
+#-----------------------------------------------------------------------
+function(set_test_properties TARGET)
+
+  target_include_directories(${TARGET} PUBLIC
+    ${OVSP4RT_INCLUDE_DIR}
+    ${SIDECAR_SOURCE_DIR}
+    ${STRATUM_SOURCE_DIR}
+    ${CMAKE_CURRENT_BINARY_DIR}
+  )
+
+  target_link_libraries(${TARGET} PUBLIC
+    GTest::gtest
+    GTest::gtest_main
+    ovsp4rt
+    p4runtime_proto
+    stratum_utils
+  )
+
+  if(TEST_COVERAGE)
+      target_compile_options(${TARGET} PRIVATE
+          -fprofile-arcs
+          -ftest-coverage
+      )
+      target_link_libraries(${TARGET} PUBLIC gcov)
+  endif()
+
+  add_test(NAME ${TARGET} COMMAND ${TARGET})
+
+endfunction(set_test_properties)
+
