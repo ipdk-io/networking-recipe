16992dde5201bf683f5a1426d26815543400f35f
Derek G Foster <derek.foster@intel.com>
2024-07-08 11:33:27 -0700

    Support P4OVS build modes (#515)
    - Updated cmake listfiles to support a `P4OVS_MODE` variable, which
      specifies the P4OVS build mode.
    
    - Modified P4CP listfile to distinguish between legacy and non-legacy
      P4OVS build modes.
    
    - Modified `make-all.sh` to accept a new `--p4ovs=MODE` parameter
      that specifies the P4OVS build mode. It passes the mode to the P4CP
      and OVS cmake builds via the `P4OVS_MODE` cmake variable.
    
    - Modified OVS listfile to accept the `P4OVS_MODE` cmake variable
      and translate it to a `--with-p4ovs` or `--with-ovsp4rt` autotools
      configuration option.
    
    - Modified `ovs/make-ovs.sh` to accept a the `--p4ovs=MODE` parameter
      and pass its value to the OVS cmake build via the `P4OVS_MODE`
      cmake variable. Also implemented `--help` and `--prefix` options.
    
    - Updated OVS submodule reference to a commit that supports the
      `--with-ovsp4rt` configuration option. This feature was implemented
      in https://github.com/ipdk-io/ovs/pull/124 and subsequent PRs.
    
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- make-all.sh
+++ make-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2022-2023 Intel Corporation
+# Copyright 2022-2024 Intel Corporation
 # SPDX-License-Identifier: Apache 2.0
 #
 # Sample script to configure and build P4 Control Plane.
@@ -17,17 +17,23 @@ _DEPS_DIR=${DEPEND_INSTALL}
 _HOST_DIR=${HOST_INSTALL}
 _NJOBS=8
 _OVS_DIR="${OVS_INSTALL:-ovs/install}"
+_P4OVS_MODE=P4OVS
 _PREFIX="install"
 _RPATH=OFF
 _SDE_DIR=${SDE_INSTALL}
 _TGT_TYPE="DPDK"
 _TOOLFILE=${CMAKE_TOOLCHAIN_FILE}
+_WITH_KRNLMON=ON
+_WITH_OVSP4RT=ON
 
 _BLD_DIR=build
-_DO_BUILD=1
-_DRY_RUN=0
 _OVS_BLD="ovs/build"
-_WITH_OVS=1
+
+_do_build=1
+_dry_run=0
+_ovs_first=0
+_ovs_last=0
+_with_ovsp4rt=1
 
 ##############
 # print_help #
@@ -57,13 +63,20 @@ print_help() {
     echo "  --no-build           Configure without building"
     echo "  --no-krnlmon         Exclude Kernel Monitor"
     echo "  --no-ovs             Exclude OVS support"
+    echo "  --p4ovs=MODE         Build OvS in specified P4OVS mode"
     echo "  --target=TARGET      Target to build (dpdk|es2k|tofino) [${_TGT_TYPE}]"
     echo ""
-    echo "Configurations:"
+    echo "Build types:"
     echo "  --debug              Debug configuration"
     echo "  --minsize            MinSizeRel configuration"
     echo "  --reldeb             RelWithDebInfo configuration"
     echo "  --release            Release configuration"
+    echo ""
+    echo "P4OVS modes:"
+    echo "  none                 Build OvS in non-P4 mode"
+    echo "  ovsp4rt              Build OvS with ovsp4rt library"
+    echo "  p4ovs                Build OvS in legacy P4 mode (default)"
+    echo "  stubs                Build OVS with ovsp4rt stubs"
     echo ""
     echo "Environment variables:"
     echo "  CMAKE_TOOLCHAIN_FILE - Default toolchain file"
@@ -80,28 +93,42 @@ print_help() {
 
 print_cmake_params() {
     echo ""
-    [ -n "${_GENERATOR}" ] && echo "${_GENERATOR}"
-    [ -n "${_BUILD_TYPE}" ] && echo "${_BUILD_TYPE:2}"
-    echo "CMAKE_INSTALL_PREFIX=${_PREFIX}"
-    [ -n "${_STAGING_PREFIX}" ] && echo "${_STAGING_PREFIX:2}"
-    [ -n "${_TOOLCHAIN_FILE}" ] && echo "${_TOOLCHAIN_FILE:2}"
+    echo "Networking-recipe options:"
+    [ -n "${_BUILD_TYPE}" ] && echo "  ${_BUILD_TYPE:2}"
+    echo "  CMAKE_INSTALL_PREFIX=${_PREFIX}"
+    [ -n "${_COVERAGE}" ] && echo "  ${_COVERAGE:2}"
     [ -n "${_CXX_STD}" ] && echo "CMAKE_CXX_STANDARD=${_CXX_STD}"
-    echo "DEPEND_INSTALL_DIR=${_DEPS_DIR}"
-    [ -n "${_HOST_DEPEND_DIR}" ] && echo "${_HOST_DEPEND_DIR:2}"
-    echo "OVS_INSTALL_DIR=${_OVS_DIR}"
-    echo "SDE_INSTALL_DIR=${_SDE_DIR}"
-    [ -n "${_WITH_KRNLMON}" ] && echo "${_WITH_KRNLMON:2}"
-    [ -n "${_WITH_OVSP4RT}" ] && echo "${_WITH_OVSP4RT:2}"
-    [ -n "${_COVERAGE}" ] && echo "${_COVERAGE:2}"
-    echo "${_SET_RPATH:2}"
-    echo "${_TARGET_TYPE:2}"
-    if [ ${_DO_BUILD} -eq 0 ]; then
+    echo "  DEPEND_INSTALL_DIR=${_DEPS_DIR}"
+    [ -n "${_GENERATOR}" ] && echo "  ${_GENERATOR}"
+    [ -n "${_HOST_DEPEND_DIR}" ] && echo "  ${_HOST_DEPEND_DIR:2}"
+    echo "  JOBS=${_NJOBS}"
+    echo "  OVS_INSTALL_DIR=${_OVS_DIR}"
+    [ -n "${_P4OVS_MODE}" ] && echo "  ${_P4OVS_MODE:2}"
+    echo "  SDE_INSTALL_DIR=${_SDE_DIR}"
+    echo "  ${_SET_RPATH:2}"
+    [ -n "${_STAGING_PREFIX}" ] && echo "  ${_STAGING_PREFIX:2}"
+    echo "  ${_TARGET_TYPE:2}"
+    [ -n "${_TOOLCHAIN_FILE}" ] && echo "  ${_TOOLCHAIN_FILE:2}"
+    [ -n "${_WITH_KRNLMON}" ] && echo "  ${_WITH_KRNLMON:2}"
+    [ -n "${_WITH_OVSP4RT}" ] && echo "  ${_WITH_OVSP4RT:2}"
+
+    echo ""
+    echo "OvS options:"
+    [ -n "${_P4OVS_MODE}" ] && echo "  ${_P4OVS_MODE:2}"
+    [ -n "${_PKG_CONFIG_PATH}" ] && echo "  PKG_CONFIG_PATH=${_PKG_CONFIG_PATH}"
+    if [ ${_ovs_first} -ne 0 ]; then
+        echo "  OVS will be built first"
+    elif [ ${_ovs_last} -ne 0 ]; then
+        echo "  OVS will be built last"
+    else
+        echo "  OVS will not be built"
+    fi
+
+    if [ ${_do_build} -eq 0 ]; then
         echo ""
         echo "Configure without building"
-        echo ""
-        return
     fi
-    echo "JOBS=${_NJOBS}"
+
     echo ""
 }
 
@@ -110,12 +137,16 @@ print_cmake_params() {
 ##############
 
 config_ovs() {
+    if [ -n "${_PKG_CONFIG_PATH}" ]; then
+        export PKG_CONFIG_PATH="${_PKG_CONFIG_PATH}"
+    fi
+
     # shellcheck disable=SC2086
     cmake -S ovs -B ${_OVS_BLD} \
         ${_BUILD_TYPE}  \
         -DCMAKE_INSTALL_PREFIX="${_OVS_DIR}" \
         ${_TOOLCHAIN_FILE} \
-        -DP4OVS=ON
+        ${_P4OVS_MODE}
 }
 
 #############
@@ -145,6 +176,7 @@ config_recipe() {
         -DSDE_INSTALL_DIR="${_SDE_DIR}" \
         ${_WITH_KRNLMON} \
         ${_WITH_OVSP4RT} \
+        ${_P4OVS_MODE} \
         ${_COVERAGE} \
         ${_SET_RPATH} \
         ${_TARGET_TYPE}
@@ -155,6 +187,7 @@ config_recipe() {
 ################
 
 build_recipe() {
+    [ ${_do_build} -eq 0 ] && return
     cmake --build ${_BLD_DIR} "-j${_NJOBS}" --target install
 }
 
@@ -166,7 +199,7 @@ SHORTOPTS=D:H:O:P:S:T:
 SHORTOPTS=${SHORTOPTS}hj:n
 
 LONGOPTS=deps:,hostdeps:,ovs:,prefix:,sde:,toolchain:
-LONGOPTS=${LONGOPTS},cxx-std:,jobs:,staging:,target:
+LONGOPTS=${LONGOPTS},cxx-std:,jobs:,p4ovs:,staging:,target:
 LONGOPTS=${LONGOPTS},debug,release,minsize,reldeb
 LONGOPTS=${LONGOPTS},dry-run,help
 LONGOPTS=${LONGOPTS},coverage,ninja,rpath
@@ -220,7 +253,7 @@ while true ; do
         _CXX_STD=$2
         shift 2;;
     --dry-run|-n)
-        _DRY_RUN=1
+        _dry_run=1
         shift ;;
     --help|-h)
         print_help
@@ -232,18 +265,22 @@ while true ; do
         _GENERATOR="-G Ninja"
         shift 1 ;;
     --no-build)
-        _DO_BUILD=0
+        _do_build=0
         shift ;;
     --no-krnlmon)
-        _WITH_KRNLMON=FALSE
+        _WITH_KRNLMON=OFF
         shift ;;
     --no-ovs)
-        _WITH_OVS=0
-        _WITH_OVSP4RT=FALSE
+        _WITH_OVSP4RT=OFF
+        _with_ovsp4rt=0
         shift ;;
     --no-rpath)
         _RPATH=OFF
         shift ;;
+    --p4ovs)
+        # convert to uppercase
+        _P4OVS_MODE=${2^^}
+        shift 2 ;;
     --rpath)
         _RPATH=ON
         shift ;;
@@ -269,9 +306,36 @@ if [ -z "${_SDE_DIR}" ]; then
     exit 1
 fi
 
+config_legacy_mode() {
+    _ovs_first=1
+}
+
+config_non_legacy_mode() {
+    _ovs_last=1
+    local _PKG_CONFIG_DIR
+    _PKG_CONFIG_DIR="$(realpath -m "${_PREFIX}"/lib/pkgconfig)"
+    _PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${_PKG_CONFIG_DIR}"
+}
+
+if [ ${_with_ovsp4rt} -eq 0 ]; then
+    _P4OVS_MODE=NONE
+elif [ "${_P4OVS_MODE}" == "NONE" ]; then
+    _WITH_OVSP4RT=OFF
+elif [ "${_P4OVS_MODE}" == "P4OVS" ]; then
+    config_legacy_mode
+elif [ "${_P4OVS_MODE}" == "OVSP4RT" ]; then
+    config_non_legacy_mode
+elif [ "${_P4OVS_MODE}" == "STUBS" ]; then
+    config_non_legacy_mode
+else
+    echo "Unknown P4OVS MODE: '${_P4OVS_MODE}'"
+    exit 1
+fi
+
 [ -n "${_BLD_TYPE}" ] && _BUILD_TYPE="-DCMAKE_BUILD_TYPE=${_BLD_TYPE}"
 [ -n "${_CXX_STD}" ] && _CXX_STANDARD="-DCMAKE_CXX_STANDARD=${_CXX_STD}"
 [ -n "${_HOST_DIR}" ] && _HOST_DEPEND_DIR="-DHOST_DEPEND_DIR=${_HOST_DIR}"
+[ -n "${_P4OVS_MODE}" ] && _P4OVS_MODE="-DP4OVS_MODE=${_P4OVS_MODE}"
 [ -n "${_RPATH}" ] && _SET_RPATH="-DSET_RPATH=${_RPATH}"
 [ -n "${_STAGING}" ] && _STAGING_PREFIX="-DCMAKE_STAGING_PREFIX=${_STAGING}"
 [ -n "${_TGT_TYPE}" ] && _TARGET_TYPE="-DTDI_TARGET=${_TGT_TYPE}"
@@ -280,7 +344,7 @@ fi
 [ -n "${_WITH_OVSP4RT}" ] && _WITH_OVSP4RT="-DWITH_OVSP4RT=${_WITH_OVSP4RT}"
 
 # Show parameters if this is a dry run
-if [ ${_DRY_RUN} -ne 0 ]; then
+if [ ${_dry_run} -ne 0 ]; then
     print_cmake_params
     exit 0
 fi
@@ -289,13 +353,20 @@ fi
 # Do the build #
 ################
 
-# First build OVS
-if [ ${_WITH_OVS} -ne 0 ]; then
+# Build OVS before recipe (legacy mode)
+if [ ${_ovs_first} -ne 0 ]; then
     config_ovs
     build_ovs
 fi
 
+# Build networking recipe
 config_recipe
-if [ ${_DO_BUILD} -ne 0 ]; then
-    build_recipe
+build_recipe
+
+# Build OVS after recipe
+if [ ${_ovs_last} -ne 0 ]; then
+    config_ovs
+    if [ ${_do_build} -ne 0 ]; then
+        build_ovs
+    fi
 fi
