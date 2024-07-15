3ea5c2c81f6da20a6ede88fd6a7bb3adbbab08eb
Derek Foster <derek.foster@intel.com>
2024-07-15 16:30:42 -0700

    Revised geneve_encap_v6_table_entry_test
    - Modified test to use new Ipv6Tunnel base class.
    
    - Added TABLE_ID and VID checks.
    
    - Updated SRC_PORT check to test for workaround value.
    
    - Changed buildsystem to use ipv6_test_utils.
    
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- ovs-p4rt/sidecar/testing/geneve_encap_v6_table_entry_test.cc
+++ ovs-p4rt/sidecar/testing/geneve_encap_v6_table_entry_test.cc
@@ -5,31 +5,30 @@
 
 #include <string>
 
-#include "absl/flags/flag.h"
 #include "absl/types/optional.h"
 #include "gtest/gtest.h"
 #include "ovsp4rt/ovs-p4rt.h"
 #include "ovsp4rt_private.h"
-#include "testing/ipv6_tunnel_test_defs.h"
-#include "testing/table_entry_test.h"
+#include "testing/ipv6_tunnel_test.h"
 
 namespace ovs_p4rt {
 
 //----------------------------------------------------------------------
-// PrepareV6GeneveEncapTableEntry
+// PrepareV6GeneveEncapTableEntry()
 //----------------------------------------------------------------------
 
-// constexpr uint32_t GENEVE_ENCAP_V6_MOD_TABLE_ID = 42283616U;
-constexpr uint32_t GENEVE_ENCAP_V6_ACTION_ID = 29610186U;
+constexpr uint32_t TABLE_ID = 42283616U;
+constexpr uint32_t ACTION_ID = 29610186U;
 
 enum {
   SRC_PORT_PARAM_ID = 7,
   DST_PORT_PARAM_ID = 8,
+  VNI_PARAM_ID = 9,
 };
 
 //----------------------------------------------------------------------
 
-TEST_F(TableEntryTest, geneve_v6_encap_params_are_correct) {
+TEST_F(Ipv6TunnelTest, geneve_encap_v6_params_are_correct) {
   struct tunnel_info tunnel_info = {0};
   p4::v1::TableEntry table_entry;
   constexpr bool insert_entry = true;
@@ -43,17 +42,19 @@ TEST_F(TableEntryTest, geneve_v6_encap_params_are_correct) {
   DumpTableEntry(table_entry);
 
   // Assert
-  ASSERT_TRUE(table_entry.has_action());
+  ASSERT_EQ(table_entry.table_id(), TABLE_ID);
 
+  ASSERT_TRUE(table_entry.has_action());
   auto table_action = table_entry.action();
   auto action = table_action.action();
-  ASSERT_EQ(action.action_id(), GENEVE_ENCAP_V6_ACTION_ID);
+  ASSERT_EQ(action.action_id(), ACTION_ID);
 
   auto params = action.params();
   int num_params = action.params_size();
 
   absl::optional<uint16_t> src_port;
   absl::optional<uint16_t> dst_port;
+  absl::optional<uint16_t> vni;
 
   for (int i = 0; i < num_params; ++i) {
     auto param = params[i];
@@ -63,20 +64,25 @@ TEST_F(TableEntryTest, geneve_v6_encap_params_are_correct) {
       src_port = DecodeWordValue(param_value) & 0xffff;
     } else if (param_id == DST_PORT_PARAM_ID) {
       dst_port = DecodeWordValue(param_value) & 0xffff;
+    } else if (param_id == VNI_PARAM_ID) {
+      vni = DecodeWordValue(param_value) & 0xffff;
     }
   }
 
-  if (absl::GetFlag(FLAGS_check_src_port)) {
-    // The src_port field contains an arbitrary value that has nothing
-    // to do with what was specified in the tunnel_info structure.
-    // It is a workaround for a long-standing bug in the Linux
-    // Networking P4 program.
-    ASSERT_TRUE(src_port.has_value());
-    ASSERT_EQ(src_port.value(), SRC_PORT);
+  ASSERT_TRUE(src_port.has_value());
+
+  if (check_src_port_) {
+    // To work around a bug in the Linux Networking P4 program, we
+    // ignore the src_port value specified by the caller and instead
+    // set the src_port param to (dst_port * 2).
+    EXPECT_EQ(src_port.value(), DST_PORT * 2);  // SRC_PORT
   }
 
   ASSERT_TRUE(dst_port.has_value());
   ASSERT_EQ(dst_port.value(), DST_PORT);
+
+  ASSERT_TRUE(vni.has_value());
+  EXPECT_EQ(vni.value(), VNI);
 }
 
 }  // namespace ovs_p4rt
