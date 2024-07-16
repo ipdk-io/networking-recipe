dbe9a5fdbe544fac93614b0faee65e0a7974c45e
Derek Foster <derek.foster@intel.com>
2024-07-15 17:26:53 -0700

    Update vxlan_encap_v4_table_entry_test
    - Modified test to use new Ipv4Tunnel base class.
    
    - Added TABLE_ID and VID checks.
    
    - Updated SRC_PORT check to test for workaround value.
    
    - Changed buildsystem to use ipv4_test_utils.
    
    - Ordered cmake test targets.
    
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- ovs-p4rt/sidecar/testing/vxlan_encap_v4_table_entry_test.cc
+++ ovs-p4rt/sidecar/testing/vxlan_encap_v4_table_entry_test.cc
@@ -1,110 +1,33 @@
 // Copyright 2024 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-#include <arpa/inet.h>
 #include <stdint.h>
 
-#include <iostream>
-#include <string>
-
-#include "absl/flags/flag.h"
 #include "absl/types/optional.h"
-#include "google/protobuf/util/json_util.h"
 #include "gtest/gtest.h"
 #include "ovsp4rt/ovs-p4rt.h"
 #include "ovsp4rt_private.h"
-#include "p4/config/v1/p4info.pb.h"
-#include "p4info_text.h"
-#include "stratum/lib/utils.h"
-
-ABSL_FLAG(bool, dump_json, false, "Dump output object in JSON");
-ABSL_FLAG(bool, check_src_port, false, "Verify src_port field");
+#include "testing/ipv4_tunnel_test.h"
 
 namespace ovs_p4rt {
-
-using google::protobuf::util::JsonPrintOptions;
-using google::protobuf::util::MessageToJsonString;
-using stratum::ParseProtoFromString;
-
-static ::p4::config::v1::P4Info p4info;
-
-class VxlanEncapTableEntryTest : public ::testing::Test {
- protected:
-  VxlanEncapTableEntryTest() { dump_json_ = absl::GetFlag(FLAGS_dump_json); };
-  static void SetUpTestSuite() {
-    ::util::Status status = ParseProtoFromString(P4INFO_TEXT, &p4info);
-    if (!status.ok()) {
-      std::exit(EXIT_FAILURE);
-    }
-  }
-
-  void DumpTableEntry(const ::p4::v1::TableEntry& table_entry) {
-    if (dump_json_) {
-      JsonPrintOptions options;
-      options.add_whitespace = true;
-      options.preserve_proto_field_names = true;
-      std::string output;
-      ASSERT_TRUE(MessageToJsonString(table_entry, &output, options).ok());
-      std::cout << output << std::endl;
-    }
-  }
-  bool dump_json_;
-};
-
-uint32_t DecodeWordValue(const std::string& string_value) {
-  uint32_t word_value = 0;
-  for (int i = 0; i < string_value.size(); i++) {
-    word_value = (word_value << 8) | (string_value[i] & 0xff);
-  }
-  return word_value;
-}
 
 //----------------------------------------------------------------------
 // PrepareVxlanEncapTableEntry()
 //----------------------------------------------------------------------
 
-constexpr char IPV4_SRC_ADDR[] = "10.0.0.1";
-constexpr char IPV4_DST_ADDR[] = "192.168.17.5";
-constexpr int IPV4_PREFIX_LEN = 24;
-
-constexpr uint16_t SRC_PORT = 4;
-constexpr uint16_t DST_PORT = 1984;
-constexpr uint16_t VNI = 0x101;
-
-constexpr uint16_t IPV4_SRC_PORT = 4;
-constexpr uint16_t IPV4_DST_PORT = 1984;
-constexpr uint16_t IPV4_VNI = 0x101;
-
-constexpr uint32_t VXLAN_ENCAP_MOD_TABLE_ID = 40763773U;
-constexpr uint32_t VXLAN_ENCAP_ACTION_ID = 20733968U;
-
-enum {
-  SRC_PORT_PARAM_ID = 3,
-  DST_PORT_PARAM_ID = 4,
-};
-
-//----------------------------------------------------------------------
-
-void InitV4TunnelInfo(tunnel_info& info) {
-  ASSERT_EQ(inet_pton(AF_INET, IPV4_SRC_ADDR, &info.local_ip.ip.v4addr.s_addr),
-            1);
-  info.local_ip.prefix_len = IPV4_PREFIX_LEN;
-
-  ASSERT_EQ(inet_pton(AF_INET, IPV4_DST_ADDR, &info.remote_ip.ip.v4addr.s_addr),
-            1);
-  info.remote_ip.prefix_len = IPV4_PREFIX_LEN;
-
-  info.src_port = htons(SRC_PORT);
-  info.dst_port = htons(DST_PORT);
-  info.vni = htons(VNI);
-}
-
-//----------------------------------------------------------------------
-
-TEST_F(VxlanEncapTableEntryTest, vxlan_encap_v4_params_are_correct) {
+TEST_F(Ipv4TunnelTest, vxlan_encap_v4_params_are_correct) {
   struct tunnel_info tunnel_info = {0};
   p4::v1::TableEntry table_entry;
   constexpr bool insert_entry = true;
+
+  constexpr uint32_t TABLE_ID = 40763773U;
+  constexpr uint32_t ACTION_ID = 20733968U;
+
+  enum {
+    SRC_PORT_PARAM_ID = 3,
+    DST_PORT_PARAM_ID = 4,
+    VNI_PARAM_ID = 5,
+  };
 
   // Arrange
   InitV4TunnelInfo(tunnel_info);
@@ -114,17 +37,19 @@ TEST_F(VxlanEncapTableEntryTest, vxlan_encap_v4_params_are_correct) {
   DumpTableEntry(table_entry);
 
   // Assert
+  ASSERT_EQ(table_entry.table_id(), TABLE_ID);
   ASSERT_TRUE(table_entry.has_action());
 
   auto table_action = table_entry.action();
   auto action = table_action.action();
-  ASSERT_EQ(action.action_id(), VXLAN_ENCAP_ACTION_ID);
+  ASSERT_EQ(action.action_id(), ACTION_ID);
 
   auto params = action.params();
   int num_params = action.params_size();
 
   absl::optional<uint16_t> src_port;
   absl::optional<uint16_t> dst_port;
+  absl::optional<uint16_t> vni;
 
   for (int i = 0; i < num_params; ++i) {
     auto param = params[i];
@@ -134,20 +59,25 @@ TEST_F(VxlanEncapTableEntryTest, vxlan_encap_v4_params_are_correct) {
       src_port = DecodeWordValue(param_value) & 0xffff;
     } else if (param_id == DST_PORT_PARAM_ID) {
       dst_port = DecodeWordValue(param_value) & 0xffff;
+    } else if (param_id == VNI_PARAM_ID) {
+      vni = DecodeWordValue(param_value) & 0xffff;
     }
   }
 
-  if (absl::GetFlag(FLAGS_check_src_port)) {
-    // The src_port param is an arbitrary value that has nothing
-    // to do with what was specified in the tunnel_info structure.
-    // It is a workaround for a long-standing bug in the Linux
-    // Networking P4 program.
-    ASSERT_TRUE(src_port.has_value());
-    EXPECT_EQ(src_port.value(), SRC_PORT);
+  ASSERT_TRUE(src_port.has_value());
+
+  if (check_src_port_) {
+    // To work around a bug in the Linux Networking P4 program, we
+    // ignore the src_port value specified by the caller and instead
+    // set the src_port param to (dst_port * 2).
+    EXPECT_EQ(src_port.value(), DST_PORT * 2);  // SRC_PORT
   }
 
   ASSERT_TRUE(dst_port.has_value());
   EXPECT_EQ(dst_port.value(), DST_PORT);
+
+  ASSERT_TRUE(vni.has_value());
+  EXPECT_EQ(vni.value(), VNI);
 }
 
 }  // namespace ovs_p4rt
