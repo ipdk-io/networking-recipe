80247a8f9fd186570c1376b23fdc2939477b8250
Derek Foster <derek.foster@intel.com>
2024-08-01 06:59:10 -0700

    Continue development
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- ovs-p4rt/sidecar/testing/prepare_fdb_smac_entry_test.cc
+++ ovs-p4rt/sidecar/testing/prepare_fdb_smac_entry_test.cc
@@ -70,75 +70,54 @@ class MacLearnInfoTest : public ::testing::Test {
   void InitFdbInfo() {
     constexpr uint8_t MAC_ADDR[] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66};
     constexpr uint8_t BRIDGE_ID = 42;
+
+    memcpy(fdb_info.mac_addr, MAC_ADDR, sizeof(fdb_info.mac_addr));
+    fdb_info.bridge_id = BRIDGE_ID;
   }
-
-#if 0
-  void InitV4TunnelInfo() {
-    constexpr char IPV4_SRC_ADDR[] = "10.20.30.40";
-    constexpr char IPV4_DST_ADDR[] = "192.168.17.5";
-    constexpr int IPV4_PREFIX_LEN = 24;
-
-    EXPECT_EQ(inet_pton(AF_INET, IPV4_SRC_ADDR,
-                        &tunnel_info.local_ip.ip.v4addr.s_addr),
-              1)
-        << "Error converting " << IPV4_SRC_ADDR;
-    tunnel_info.local_ip.family = AF_INET;
-    tunnel_info.local_ip.prefix_len = IPV4_PREFIX_LEN;
-
-    EXPECT_EQ(inet_pton(AF_INET, IPV4_DST_ADDR,
-                        &tunnel_info.remote_ip.ip.v4addr.s_addr),
-              1)
-        << "Error converting " << IPV4_DST_ADDR;
-    tunnel_info.remote_ip.family = AF_INET;
-    tunnel_info.remote_ip.prefix_len = IPV4_PREFIX_LEN;
-
-    tunnel_info.bridge_id = 86;
-  }
-
-  void InitVxlanTagged() {
-    tunnel_info.tunnel_type = OVS_TUNNEL_VXLAN;
-    tunnel_info.vlan_info.port_vlan_mode = P4_PORT_VLAN_NATIVE_TAGGED;
-    tunnel_info.vni = 0x1776B;
-    ACTION_ID = GetActionId(SET_VXLAN_DECAP_OUTER_HDR);
-  }
-#endif
 
   void CheckActions() const {}
 
   void CheckMatches() const {
-    constexpr int MAC_ADDR_SIZE = 6;
-    constexpr int BRIDGE_ID_SIZE = 1;
-
     constexpr int MFID_MAC_ADDR = 1;
     constexpr int MFID_BRIDGE_ID = 2;
 
     // number of match fields
     ASSERT_EQ(table_entry.match_size(), 2);
 
-    {
-      // match field 1: sa
-      auto match = table_entry.match()[0];
-      ASSERT_EQ(match.field_id(), MFID_MAC_ADDR);
-      ASSERT_TRUE(match.has_exact());
-
-      auto match_value = match.exact().value();
-      ASSERT_EQ(match_value.size(), MAC_ADDR_SIZE);
-
-      for (int i = 0; i < MAC_ADDR_SIZE; i++) {
-        EXPECT_EQ(uint8_t(match_value[i]), fdb_info.mac_addr[i]);
+    for (const auto& match : table_entry.match()) {
+      auto field_id = match.field_id();
+      if (field_id == MFID_MAC_ADDR) {
+        CheckMacAddr(match);
+      } else if (field_id == MFID_BRIDGE_ID) {
+        CheckBridgeId(match);
+      } else {
+        FAIL() << "Unexpected field_id (" << field_id << ")";
       }
     }
+  }
 
-    {
-      // match field 2: bridge_id
-      auto match = table_entry.match()[1];
-      ASSERT_EQ(match.field_id(), MFID_BRIDGE_ID);
-      ASSERT_TRUE(match.has_exact());
+  void CheckMacAddr(const ::p4::v1::FieldMatch& match) const {
+    constexpr int MAC_ADDR_SIZE = 6;
 
-      auto match_value = match.exact().value();
-      ASSERT_EQ(match_value.size(), BRIDGE_ID_SIZE);
-      ASSERT_EQ(uint8_t(match_value[0]), fdb_info.tnl_info.bridge_id);
+    ASSERT_TRUE(match.has_exact());
+    const auto& match_value = match.exact().value();
+    ASSERT_EQ(match_value.size(), MAC_ADDR_SIZE);
+
+    for (int i = 0; i < MAC_ADDR_SIZE; i++) {
+      EXPECT_EQ(match_value[i], fdb_info.mac_addr[i])
+          << "mac_addr[" << i << "] is incorrect";
     }
+  }
+
+  void CheckBridgeId(const ::p4::v1::FieldMatch& match) const {
+    constexpr int BRIDGE_ID_SIZE = 1;
+
+    ASSERT_TRUE(match.has_exact());
+    const auto& match_value = match.exact().value();
+    ASSERT_EQ(match_value.size(), BRIDGE_ID_SIZE);
+
+    // widen so values will be treated as ints
+    ASSERT_EQ(uint32_t(match_value[0]), uint32_t(fdb_info.bridge_id));
   }
 
   void CheckDetail() const {
@@ -152,12 +131,12 @@ class MacLearnInfoTest : public ::testing::Test {
     CheckActions();
   }
 
-  // Test case variables
+  // Working variables
   struct mac_learning_info fdb_info = {0};
   ::p4::v1::TableEntry table_entry;
   DiagDetail detail;
 
-  // Values to check against
+  // Comparison variables
   uint32_t TABLE_ID;
 
  private:
@@ -179,12 +158,12 @@ class MacLearnInfoTest : public ::testing::Test {
 // PrepareFdbSmacTableEntry
 //----------------------------------------------------------------------
 
-TEST_F(MacLearnInfoTest, remove_table_entry_is_correct) {
+TEST_F(MacLearnInfoTest, PrepareFdbSmacTableEntry) {
   // Arrange
   InitFdbInfo();
 
   // Act
-  PrepareFdbSmacTableEntry(&table_entry, fdb_info, p4info, REMOVE_ENTRY,
+  PrepareFdbSmacTableEntry(&table_entry, fdb_info, p4info, INSERT_ENTRY,
                            detail);
 
   // Assert
