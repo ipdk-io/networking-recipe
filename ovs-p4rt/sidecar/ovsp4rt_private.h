7ed83ae6997e5b67eb300b29a1187bef822b0000
Derek G Foster <derek.foster@intel.com>
2024-08-05 07:45:53 -0700

    [ovsp4rt] Implement fdb_rx_vlan_entry_test (#593)
    - Unit test for PrepareFdbRxVlanTableEntry().
    
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- ovs-p4rt/sidecar/ovsp4rt_private.h
+++ ovs-p4rt/sidecar/ovsp4rt_private.h
@@ -35,6 +35,11 @@ void PrepareVxlanEncapTableEntry(p4::v1::TableEntry* table_entry,
 
 #if defined(ES2K_TARGET)
 
+void PrepareFdbRxVlanTableEntry(p4::v1::TableEntry* table_entry,
+                                const struct mac_learning_info& learn_info,
+                                const ::p4::config::v1::P4Info& p4info,
+                                bool insert_entry, DiagDetail& detail);
+
 void PrepareFdbSmacTableEntry(p4::v1::TableEntry* table_entry,
                               const struct mac_learning_info& learn_info,
                               const ::p4::config::v1::P4Info& p4info,
