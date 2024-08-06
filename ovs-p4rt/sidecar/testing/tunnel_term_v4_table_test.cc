88abf73ae3409f0b6a4fccc8ae781b5341ca8a3c
Derek Foster <derek.foster@intel.com>
2024-08-06 06:22:41 -0700

    Modified to use P4InfoHelper class
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- ovs-p4rt/sidecar/testing/tunnel_term_v4_table_test.cc
+++ ovs-p4rt/sidecar/testing/tunnel_term_v4_table_test.cc
@@ -1,11 +1,6 @@
 // Copyright 2024 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-// NOTE:
-// This is a minimal unit test, used solely to check the table_id
-// field. It needs to be expanded to validate all output fields
-// for all (tunnel_type, vlan_mode) combinations.
-
 #define DUMP_JSON
 
 #include <arpa/inet.h>
@@ -23,6 +18,7 @@
 #include "ovsp4rt_private.h"
 #include "p4/config/v1/p4info.pb.h"
 #include "p4/v1/p4runtime.pb.h"
+#include "p4info_helper.h"
 #include "p4info_text.h"
 #include "stratum/lib/utils.h"
 
@@ -45,7 +41,7 @@ static ::p4::config::v1::P4Info p4info;
 
 class TunnelTermV4TableTest : public ::testing::Test {
  protected:
-  TunnelTermV4TableTest() {
+  TunnelTermV4TableTest() : helper(p4info) {
 #ifdef DUMP_JSON
     dump_json_ = absl::GetFlag(FLAGS_dump_json);
 #endif
@@ -58,55 +54,7 @@ class TunnelTermV4TableTest : public ::testing::Test {
     }
   }
 
-  void SetUp() { SelectTable("ipv4_tunnel_term_table"); }
-
-  //----------------------------
-  // P4Info lookup methods
-  //----------------------------
-
-  int GetMatchFieldId(const std::string& mf_name) const {
-    for (const auto& mf : TABLE->match_fields()) {
-      if (mf.name() == mf_name) {
-        return mf.id();
-      }
-    }
-    std::cerr << "Match Field '" << mf_name << "' not found!\n";
-    return -1;
-  }
-
-  int GetParamId(const std::string& param_name) const {
-    for (const auto& param : ACTION->params()) {
-      if (param.name() == param_name) {
-        return param.id();
-      }
-    }
-    std::cerr << "Action Param '" << param_name << "' not found!\n";
-    return -1;
-  }
-
-  void SelectAction(const std::string& action_name) {
-    for (const auto& action : p4info.actions()) {
-      const auto& pre = action.preamble();
-      if (pre.name() == action_name || pre.alias() == action_name) {
-        ACTION = &action;
-        ACTION_ID = pre.id();
-        return;
-      }
-    }
-    std::cerr << "Action '" << action_name << "' not found!\n";
-  }
-
-  void SelectTable(const std::string& table_name) {
-    for (const auto& table : p4info.tables()) {
-      const auto& pre = table.preamble();
-      if (pre.name() == table_name || pre.alias() == table_name) {
-        TABLE = &table;
-        TABLE_ID = pre.id();
-        return;
-      }
-    }
-    std::cerr << "Table '" << table_name << "' not found!\n";
-  }
+  void SetUp() { helper.SelectTable("ipv4_tunnel_term_table"); }
 
   //----------------------------
   // Utility methods
@@ -146,7 +94,7 @@ class TunnelTermV4TableTest : public ::testing::Test {
     tunnel_info.tunnel_type = OVS_TUNNEL_GENEVE;
     tunnel_info.vlan_info.port_vlan_mode = P4_PORT_VLAN_NATIVE_TAGGED;
     tunnel_info.vni = 0x1776;
-    SelectAction(SET_GENEVE_DECAP_OUTER_HDR);
+    helper.SelectAction(SET_GENEVE_DECAP_OUTER_HDR);
   }
 
   void InitGeneveUntagged() {
@@ -155,7 +103,7 @@ class TunnelTermV4TableTest : public ::testing::Test {
     tunnel_info.tunnel_type = OVS_TUNNEL_GENEVE;
     tunnel_info.vlan_info.port_vlan_mode = P4_PORT_VLAN_NATIVE_UNTAGGED;
     tunnel_info.vni = 0x1984;
-    SelectAction(SET_GENEVE_DECAP_OUTER_AND_PUSH_VLAN);
+    helper.SelectAction(SET_GENEVE_DECAP_OUTER_AND_PUSH_VLAN);
   }
 
   void InitTunnelInfo() {
@@ -185,7 +133,7 @@ class TunnelTermV4TableTest : public ::testing::Test {
     tunnel_info.tunnel_type = OVS_TUNNEL_VXLAN;
     tunnel_info.vlan_info.port_vlan_mode = P4_PORT_VLAN_NATIVE_TAGGED;
     tunnel_info.vni = 0x1066;
-    SelectAction(SET_VXLAN_DECAP_OUTER_HDR);
+    helper.SelectAction(SET_VXLAN_DECAP_OUTER_HDR);
   }
 
   void InitVxlanUntagged() {
@@ -194,39 +142,30 @@ class TunnelTermV4TableTest : public ::testing::Test {
     tunnel_info.tunnel_type = OVS_TUNNEL_VXLAN;
     tunnel_info.vlan_info.port_vlan_mode = P4_PORT_VLAN_NATIVE_UNTAGGED;
     tunnel_info.vni = 0x1492;
-    SelectAction(SET_VXLAN_DECAP_OUTER_AND_PUSH_VLAN);
+    helper.SelectAction(SET_VXLAN_DECAP_OUTER_AND_PUSH_VLAN);
   }
 
   //----------------------------
-  // Test-specific checks
-  //----------------------------
-
-  void CheckTableEntry() const {
-    ASSERT_FALSE(TABLE == nullptr);
-    EXPECT_EQ(table_entry.table_id(), TABLE_ID);
-  }
-
-  //----------------------------
-  // Check action
+  // CheckAction()
   //----------------------------
 
   void CheckAction() const {
-    const int TUNNEL_ID = GetParamId("tunnel_id");
+    const int TUNNEL_ID = helper.GetParamId("tunnel_id");
     ASSERT_NE(TUNNEL_ID, -1);
 
     ASSERT_TRUE(table_entry.has_action());
     auto table_action = table_entry.action();
 
     auto action = table_action.action();
-    if (ACTION_ID) {
-      EXPECT_EQ(action.action_id(), ACTION_ID);
+    if (helper.action_id()) {
+      EXPECT_EQ(action.action_id(), helper.action_id());
     }
 
     auto params = action.params();
     ASSERT_EQ(action.params_size(), 1);
 
     auto param = params[0];
-    ASSERT_EQ(param.param_id(), PARAM_ID);
+    ASSERT_EQ(param.param_id(), TUNNEL_ID);
     CheckTunnelIdParam(param.value());
   }
 
@@ -244,12 +183,12 @@ class TunnelTermV4TableTest : public ::testing::Test {
   }
 
   //----------------------------
-  // Check match fields
+  // CheckMatches()
   //----------------------------
 
   void CheckMatches() const {
-    const int MFID_IPV4_SRC = GetMatchFieldId("ipv4_src");
-    const int MFID_VNI = GetMatchFieldId("vni");
+    const int MFID_IPV4_SRC = helper.GetMatchFieldId("ipv4_src");
+    const int MFID_VNI = helper.GetMatchFieldId("vni");
 
     ASSERT_NE(MFID_IPV4_SRC, -1);
     ASSERT_NE(MFID_VNI, -1);
@@ -291,24 +230,27 @@ class TunnelTermV4TableTest : public ::testing::Test {
   }
 
   //----------------------------
+  // CheckTableEntry()
+  //----------------------------
+
+  void CheckTableEntry() const {
+    ASSERT_TRUE(helper.has_table());
+    EXPECT_EQ(table_entry.table_id(), helper.table_id());
+  }
+
+  //----------------------------
   // Protected member data
   //----------------------------
 
-  // Working variables
-  ::p4::v1::TableEntry table_entry;
-  struct tunnel_info tunnel_info;
+  P4InfoHelper helper;
 
-  // Values to check against
-  uint32_t TABLE_ID;
-  int ACTION_ID = -1;
-  int PARAM_ID = 1;  // tunnel_id
+  ::p4::v1::TableEntry table_entry;
+  struct tunnel_info tunnel_info = {0};
 
  private:
   //----------------------------
   // Private member data
   //----------------------------
-  const ::p4::config::v1::Action* ACTION = nullptr;
-  const ::p4::config::v1::Table* TABLE = nullptr;
 
 #ifdef DUMP_JSON
   bool dump_json_ = false;
