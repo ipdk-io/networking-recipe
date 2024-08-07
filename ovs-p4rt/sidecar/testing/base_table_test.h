b6324886ba3a20ffec3639c47b1b14927005bd69
Derek Foster <derek.foster@intel.com>
2024-08-07 14:00:07 -0700

    Improved BaseTableTest class
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- ovs-p4rt/sidecar/testing/base_table_test.h
+++ ovs-p4rt/sidecar/testing/base_table_test.h
@@ -6,14 +6,31 @@
 
 #include <stdint.h>
 
+#include <iostream>
 #include <string>
 
+#ifdef DUMP_JSON
+#include "absl/flags/flag.h"
+#include "google/protobuf/util/json_util.h"
+#endif
 #include "gtest/gtest.h"
 #include "p4/config/v1/p4info.pb.h"
 #include "p4/v1/p4runtime.pb.h"
+#include "p4info_helper.h"
+#include "p4info_text.h"
 #include "stratum/lib/utils.h"
 
+#ifdef DUMP_JSON
+ABSL_FLAG(bool, dump_json, false, "Dump table_entry in JSON");
+#endif
+
 namespace ovsp4rt {
+
+#ifdef DUMP_JSON
+using google::protobuf::util::JsonPrintOptions;
+using google::protobuf::util::MessageToJsonString;
+#endif
+using stratum::ParseProtoFromString;
 
 constexpr bool INSERT_ENTRY = true;
 constexpr bool REMOVE_ENTRY = false;
@@ -23,47 +40,37 @@ static ::p4::config::v1::P4Info p4info;
 
 class BaseTableTest : public ::testing::Test {
  protected:
-  BaseTableTest();
+  BaseTableTest() : helper(p4info) {
+#ifdef DUMP_JSON
+    dump_json_ = absl::GetFlag(FLAGS_dump_json);
+#endif
+  }
 
   // Initializes the p4info object.
-  void SetUpTestSuite();
-
-  // Selects the table associated with the UUT.
-  void SelectTable(const std::string& table_name) {
-    for (const auto& table : p4info.tables()) {
-      const auto& pre = table.preamble();
-      if (pre.name() == table_name || pre.alias() == table_name) {
-        TABLE = &table;
-        TABLE_ID = pre.id();
-        return;
-      }
+  static void SetUpTestSuite() {
+    ::util::Status status = ParseProtoFromString(P4INFO_TEXT, &p4info);
+    if (!status.ok()) {
+      std::exit(EXIT_FAILURE);
     }
   }
 
-  // Returns the object identifier of the named action.
-  int GetActionId(const std::string& action_name) const {
-    for (const auto& action : p4info.actions()) {
-      const auto& pre = action.preamble();
-      if (pre.name() == action_name || pre.alias() == action_name) {
-        return pre.id();
-      }
-    }
-    return -1;
-  }
+  //----------------------------
+  // Utility methods
+  //----------------------------
 
-  // Returns the object identifier of the named match field.
-  int GetMatchFieldId(const std::string& mf_name) const {
-    for (const auto& mf : TABLE->match_fields()) {
-      if (mf.name() == mf_name) {
-        return mf.id();
-      }
-    }
-    return -1;
+  static uint16_t DecodePortValue(const std::string& string_value) {
+    uint16_t port_value = DecodeWordValue(string_value) & 0xffff;
+    // Port values are encoded low byte first.
+    return ntohs(port_value);
   }
 
   // Decodes a 24-bit parameter and returns its value as an unsigned integer.
   static uint32_t DecodeTunnelId(const std::string& string_value) {
     return DecodeWordValue(string_value) & 0xffffff;
+  }
+
+  static uint16_t DecodeVniValue(const std::string& string_value) {
+    return DecodeWordValue(string_value) & 0xffff;
   }
 
   // Decodes a parameter and returns its value as an unsigned integer.
@@ -75,21 +82,39 @@ class BaseTableTest : public ::testing::Test {
     return word_value;
   }
 
-  // Dumps the table entry in JSON.
-  void DumpTableEntry() const;
+  // Dumps table_entry in JSON.
+  void DumpTableEntry() const {
+#ifdef DUMP_JSON
+    if (dump_json_) {
+      JsonPrintOptions options;
+      options.add_whitespace = true;
+      options.preserve_proto_field_names = true;
+      std::string output;
+      ASSERT_TRUE(MessageToJsonString(table_entry, &output, options).ok());
+      std::cout << output << std::endl;
+    }
+#endif
+  }
+
+  //----------------------------
+  // Protected member data
+  //----------------------------
 
   // Table entry to be inserted or removed. Output of the UUT.
   ::p4::v1::TableEntry table_entry;
 
-  // Identifier of the selected table.
-  uint32_t TABLE_ID;
-
-  // Value of the --dump_json command-line flag.
-  bool dump_json_;
+  // Facilitates use of the p4info object.
+  P4InfoHelper helper;
 
  private:
-  // Pointer to selected table.
-  const ::p4::config::v1::Table* TABLE = nullptr;
+  //----------------------------
+  // Private member data
+  //----------------------------
+
+#ifdef DUMP_JSON
+  // Value of the --dump_json command-line flag.
+  bool dump_json_;
+#endif
 };
 
 }  // namespace ovsp4rt
