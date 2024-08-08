e424f1e50888af71e9c5ff069e27b946e5c72503
Derek Foster <derek.foster@intel.com>
2024-08-08 13:16:19 -0700

    [ovsp4rt] Revise geneve and vxlan encap tests
    - Defined a unique class for each encapsulation test.
    
    - Extracted a CheckAction() method for each test.
    
    - Defined a remove_entry test case for each test.
    
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- ovs-p4rt/sidecar/testing/vxlan_encap_v4_vlan_pop_test.cc
+++ ovs-p4rt/sidecar/testing/vxlan_encap_v4_vlan_pop_test.cc
@@ -11,72 +11,100 @@
 
 namespace ovsp4rt {
 
+constexpr bool INSERT_ENTRY = true;
+constexpr bool REMOVE_ENTRY = false;
+
+constexpr uint32_t TABLE_ID = 39675860U;
+constexpr uint32_t ACTION_ID = 26114242U;
+
+enum {
+  SRC_PORT_PARAM_ID = 3,
+  DST_PORT_PARAM_ID = 4,
+  VNI_PARAM_ID = 5,
+};
+
+class VxlanEncapV4VlanPopTest : public Ipv4TunnelTest {
+ protected:
+  struct tunnel_info tunnel_info = {0};
+  p4::v1::TableEntry table_entry;
+
+  void CheckAction() const {
+    ASSERT_TRUE(table_entry.has_action());
+    auto table_action = table_entry.action();
+
+    auto action = table_action.action();
+    ASSERT_EQ(action.action_id(), ACTION_ID);
+
+    auto params = action.params();
+    int num_params = action.params_size();
+
+    absl::optional<uint16_t> src_port;
+    absl::optional<uint16_t> dst_port;
+    absl::optional<uint16_t> vni;
+
+    for (int i = 0; i < num_params; ++i) {
+      auto param = params[i];
+      int param_id = param.param_id();
+      auto param_value = param.value();
+
+      if (param_id == SRC_PORT_PARAM_ID) {
+        src_port = DecodePortValue(param_value);
+      } else if (param_id == DST_PORT_PARAM_ID) {
+        dst_port = DecodePortValue(param_value);
+      } else if (param_id == VNI_PARAM_ID) {
+        vni = DecodeVniValue(param_value);
+      }
+    }
+
+    ASSERT_TRUE(src_port.has_value());
+
+    // To work around a bug in the Linux Networking P4 program, we
+    // ignore the src_port value specified by the caller and instead
+    // set the src_port param to (dst_port * 2).
+    EXPECT_EQ(src_port.value(), DST_PORT * 2);  // SRC_PORT
+
+    ASSERT_TRUE(dst_port.has_value());
+    EXPECT_EQ(dst_port.value(), DST_PORT);
+
+    ASSERT_TRUE(vni.has_value());
+    EXPECT_EQ(vni.value(), VNI);
+  }
+
+  void CheckNoAction() const {
+    ASSERT_FALSE(table_entry.has_action());
+  }
+};
+
 //----------------------------------------------------------------------
 // PrepareVxlanEncapAndVlanPopTableEntry()
 //----------------------------------------------------------------------
 
-TEST_F(Ipv4TunnelTest, vxlan_encap_v4_vlan_pop_params_are_correct) {
-  struct tunnel_info tunnel_info = {0};
-  p4::v1::TableEntry table_entry;
-  constexpr bool insert_entry = true;
-
-  constexpr uint32_t TABLE_ID = 39675860U;
-  constexpr uint32_t ACTION_ID = 26114242U;
-
-  enum {
-    SRC_PORT_PARAM_ID = 3,
-    DST_PORT_PARAM_ID = 4,
-    VNI_PARAM_ID = 5,
-  };
-
+TEST_F(VxlanEncapV4VlanPopTest, remove_entry) {
   // Arrange
   InitV4TunnelInfo(tunnel_info, OVS_TUNNEL_VXLAN);
 
   // Act
   PrepareVxlanEncapAndVlanPopTableEntry(&table_entry, tunnel_info, p4info,
-                                        insert_entry);
+                                        REMOVE_ENTRY);
   DumpTableEntry(table_entry);
 
   // Assert
   ASSERT_EQ(table_entry.table_id(), TABLE_ID);
-  ASSERT_TRUE(table_entry.has_action());
+  CheckNoAction();
+}
 
-  auto table_action = table_entry.action();
-  auto action = table_action.action();
-  ASSERT_EQ(action.action_id(), ACTION_ID);
+TEST_F(VxlanEncapV4VlanPopTest, insert_entry) {
+  // Arrange
+  InitV4TunnelInfo(tunnel_info, OVS_TUNNEL_VXLAN);
 
-  auto params = action.params();
-  int num_params = action.params_size();
+  // Act
+  PrepareVxlanEncapAndVlanPopTableEntry(&table_entry, tunnel_info, p4info,
+                                        INSERT_ENTRY);
+  DumpTableEntry(table_entry);
 
-  absl::optional<uint16_t> src_port;
-  absl::optional<uint16_t> dst_port;
-  absl::optional<uint16_t> vni;
-
-  for (int i = 0; i < num_params; ++i) {
-    auto param = params[i];
-    int param_id = param.param_id();
-    auto param_value = param.value();
-    if (param_id == SRC_PORT_PARAM_ID) {
-      src_port = DecodePortValue(param_value);
-    } else if (param_id == DST_PORT_PARAM_ID) {
-      dst_port = DecodePortValue(param_value);
-    } else if (param_id == VNI_PARAM_ID) {
-      vni = DecodeVniValue(param_value);
-    }
-  }
-
-  ASSERT_TRUE(src_port.has_value());
-
-  // To work around a bug in the Linux Networking P4 program, we
-  // ignore the src_port value specified by the caller and instead
-  // set the src_port param to (dst_port * 2).
-  EXPECT_EQ(src_port.value(), DST_PORT * 2);  // SRC_PORT
-
-  ASSERT_TRUE(dst_port.has_value());
-  EXPECT_EQ(dst_port.value(), DST_PORT);
-
-  ASSERT_TRUE(vni.has_value());
-  EXPECT_EQ(vni.value(), VNI);
+  // Assert
+  ASSERT_EQ(table_entry.table_id(), TABLE_ID);
+  CheckAction();
 }
 
 }  // namespace ovsp4rt
