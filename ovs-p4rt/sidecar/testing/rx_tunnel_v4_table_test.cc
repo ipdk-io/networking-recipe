7be8ec196430c73c25d57a38bfb550f6ccbebc0e
Derek G Foster <derek.foster@intel.com>
2024-08-09 13:19:41 -0700

    [ovsp4rt] Update tests to use BaseTableTest class (#613)
    - Modified several tests to use BaseTableTest as their parent
      class.
    
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- ovs-p4rt/sidecar/testing/rx_tunnel_v4_table_test.cc
+++ ovs-p4rt/sidecar/testing/rx_tunnel_v4_table_test.cc
@@ -9,52 +9,18 @@
 #include <iostream>
 #include <string>
 
+#include "base_table_test.h"
 #include "gtest/gtest.h"
 #include "ovsp4rt/ovs-p4rt.h"
 #include "ovsp4rt_private.h"
-#include "p4/config/v1/p4info.pb.h"
-#include "p4/v1/p4runtime.pb.h"
-#include "p4info_helper.h"
-#include "p4info_text.h"
-#include "stratum/lib/utils.h"
 
 namespace ovsp4rt {
 
-using stratum::ParseProtoFromString;
-
-constexpr bool INSERT_ENTRY = true;
-constexpr bool REMOVE_ENTRY = false;
-
-static ::p4::config::v1::P4Info p4info;
-
-class RxTunnelPortV4TableTest : public ::testing::Test {
+class RxTunnelPortV4TableTest : public BaseTableTest {
  protected:
-  RxTunnelPortV4TableTest() : helper(p4info) {}
-
-  static void SetUpTestSuite() {
-    ::util::Status status = ParseProtoFromString(P4INFO_TEXT, &p4info);
-    if (!status.ok()) {
-      std::exit(EXIT_FAILURE);
-    }
-  }
+  RxTunnelPortV4TableTest() {}
 
   void SetUp() { helper.SelectTable("rx_ipv4_tunnel_source_port"); }
-
-  //----------------------------
-  // Utility methods
-  //----------------------------
-
-  static uint16_t DecodeVniValue(const std::string& string_value) {
-    return DecodeWordValue(string_value) & 0xffff;
-  }
-
-  static uint32_t DecodeWordValue(const std::string& string_value) {
-    uint32_t word_value = 0;
-    for (int i = 0; i < string_value.size(); i++) {
-      word_value = (word_value << 8) | (string_value[i] & 0xff);
-    }
-    return word_value;
-  }
 
   //----------------------------
   // Initialization methods
@@ -164,11 +130,7 @@ class RxTunnelPortV4TableTest : public ::testing::Test {
   //----------------------------
   // Protected member data
   //----------------------------
-
-  P4InfoHelper helper;
-
   struct tunnel_info tunnel_info = {0};
-  ::p4::v1::TableEntry table_entry;
 };
 
 //----------------------------------------------------------------------
