983b9d2e72981d9442812953c316e13444f3b970
Derek Foster <derek.foster@intel.com>
2024-08-10 11:48:03 -0700

    [ovsp4rt] Update fdb_rx_vlan_entry_test
    - Modified to use BaseTableTest base class.
    
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- ovs-p4rt/sidecar/testing/fdb_rx_vlan_entry_test.cc
+++ ovs-p4rt/sidecar/testing/fdb_rx_vlan_entry_test.cc
@@ -8,124 +8,19 @@
 #include <iostream>
 #include <string>
 
-#include "absl/flags/flag.h"
-#include "google/protobuf/util/json_util.h"
+#include "base_table_test.h"
 #include "gtest/gtest.h"
 #include "logging/ovsp4rt_diag_detail.h"
 #include "ovsp4rt/ovs-p4rt.h"
 #include "ovsp4rt_private.h"
-#include "p4/config/v1/p4info.pb.h"
-#include "p4/v1/p4runtime.pb.h"
-#include "p4info_text.h"
-#include "stratum/lib/utils.h"
-
-ABSL_FLAG(bool, dump_json, false, "Dump output table_entry in JSON");
 
 namespace ovsp4rt {
 
-using google::protobuf::util::JsonPrintOptions;
-using google::protobuf::util::MessageToJsonString;
-using stratum::ParseProtoFromString;
-
-constexpr bool INSERT_ENTRY = true;
-constexpr bool REMOVE_ENTRY = false;
-
-static ::p4::config::v1::P4Info p4info;
-
-class FdbRxVlanEntryTest : public ::testing::Test {
+class FdbRxVlanEntryTest : public BaseTableTest {
  protected:
-  FdbRxVlanEntryTest() { dump_json_ = absl::GetFlag(FLAGS_dump_json); }
-
-  static void SetUpTestSuite() {
-    ::util::Status status = ParseProtoFromString(P4INFO_TEXT, &p4info);
-    if (!status.ok()) {
-      std::exit(EXIT_FAILURE);
-    }
-  }
+  FdbRxVlanEntryTest() {}
 
   void SetUp() { SelectTable("l2_fwd_rx_table"); }
-
-  //----------------------------
-  // P4Info lookup methods
-  //----------------------------
-
-  int GetActionId(const std::string& action_name) const {
-    for (const auto& action : p4info.actions()) {
-      const auto& pre = action.preamble();
-      if (pre.name() == action_name || pre.alias() == action_name) {
-        return pre.id();
-      }
-    }
-    std::cerr << "Action '" << action_name << "' not found!\n";
-    return -1;
-  }
-
-  int GetMatchFieldId(const std::string& mf_name) const {
-    for (const auto& mf : TABLE->match_fields()) {
-      if (mf.name() == mf_name) {
-        return mf.id();
-      }
-    }
-    std::cerr << "Match Field '" << mf_name << "' not found!\n";
-    return -1;
-  }
-
-  int GetParamId(const std::string& param_name) const {
-    for (const auto& param : ACTION->params()) {
-      if (param.name() == param_name) {
-        return param.id();
-      }
-    }
-    std::cerr << "Action Param '" << param_name << "' not found!\n";
-    return -1;
-  }
-
-  void SelectAction(const std::string& action_name) {
-    for (const auto& action : p4info.actions()) {
-      const auto& pre = action.preamble();
-      if (pre.name() == action_name || pre.alias() == action_name) {
-        ACTION = &action;
-        ACTION_ID = pre.id();
-        return;
-      }
-    }
-    std::cerr << "Action '" << action_name << "' not found!\n";
-  }
-
-  void SelectTable(const std::string& table_name) {
-    for (const auto& table : p4info.tables()) {
-      const auto& pre = table.preamble();
-      if (pre.name() == table_name || pre.alias() == table_name) {
-        TABLE = &table;
-        TABLE_ID = pre.id();
-        return;
-      }
-    }
-    std::cerr << "Table '" << table_name << "' not found!\n";
-  }
-
-  //----------------------------
-  // Utility methods
-  //----------------------------
-
-  static uint32_t DecodeWordValue(const std::string& string_value) {
-    uint32_t word_value = 0;
-    for (int i = 0; i < string_value.size(); i++) {
-      word_value = (word_value << 8) | (string_value[i] & 0xff);
-    }
-    return word_value;
-  }
-
-  void DumpTableEntry() {
-    if (dump_json_) {
-      JsonPrintOptions options;
-      options.add_whitespace = true;
-      options.preserve_proto_field_names = true;
-      std::string output;
-      ASSERT_TRUE(MessageToJsonString(table_entry, &output, options).ok());
-      std::cout << output << std::endl;
-    }
-  }
 
   //----------------------------
   // Initialization methods
@@ -150,13 +45,13 @@ class FdbRxVlanEntryTest : public ::testing::Test {
   void CheckAction() const {
     const int PORT_PARAM = GetParamId("port");
 
-    ASSERT_NE(ACTION_ID, -1);
+    ASSERT_NE(ActionId(), -1);
 
     ASSERT_TRUE(table_entry.has_action());
     const auto& table_action = table_entry.action();
 
     const auto& action = table_action.action();
-    EXPECT_EQ(action.action_id(), ACTION_ID);
+    EXPECT_EQ(action.action_id(), ActionId());
 
     auto params = action.params();
     ASSERT_EQ(action.params_size(), 1);
@@ -216,8 +111,8 @@ class FdbRxVlanEntryTest : public ::testing::Test {
   }
 
   void CheckTableEntry() const {
-    ASSERT_FALSE(TABLE == nullptr);
-    EXPECT_EQ(table_entry.table_id(), TABLE_ID);
+    ASSERT_TRUE(HasTable());
+    EXPECT_EQ(table_entry.table_id(), TableId());
   }
 
   //----------------------------
@@ -226,22 +121,7 @@ class FdbRxVlanEntryTest : public ::testing::Test {
 
   // Working variables
   struct mac_learning_info fdb_info = {0};
-  ::p4::v1::TableEntry table_entry;
   DiagDetail detail;
-
-  // Comparison variables
-  uint32_t TABLE_ID;
-  int ACTION_ID = -1;
-
- private:
-  //----------------------------
-  // Private member data
-  //----------------------------
-
-  const ::p4::config::v1::Table* TABLE = nullptr;
-  const ::p4::config::v1::Action* ACTION = nullptr;
-
-  bool dump_json_ = false;
 };
 
 //----------------------------------------------------------------------
@@ -274,6 +154,7 @@ TEST_F(FdbRxVlanEntryTest, insert_entry) {
   DumpTableEntry();
 
   // Assert
+  CheckTableEntry();
   CheckAction();
 }
 
