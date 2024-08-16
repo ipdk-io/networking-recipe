88b5f99b881b9c152bcb602d578e2cce5d2483fc
Derek Foster <derek.foster@intel.com>
2024-08-16 14:09:37 -0700

    [ovsp4rt] Revise vlan_pop_mod_table_test to use BaseTableTest
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- ovs-p4rt/sidecar/testing/vlan_pop_mod_table_test.cc
+++ ovs-p4rt/sidecar/testing/vlan_pop_mod_table_test.cc
@@ -7,54 +7,25 @@
 
 #include <string>
 
+#include "base_table_test.h"
 #include "gtest/gtest.h"
 #include "ovsp4rt/ovs-p4rt.h"
 #include "ovsp4rt_private.h"
 #include "p4/config/v1/p4info.pb.h"
-#include "p4/v1/p4runtime.pb.h"
-#include "p4info_helper.h"
-#include "p4info_text.h"
-#include "stratum/lib/utils.h"
 
 namespace ovsp4rt {
 
-using stratum::ParseProtoFromString;
-
-constexpr bool INSERT_ENTRY = true;
-constexpr bool REMOVE_ENTRY = false;
-
-static ::p4::config::v1::P4Info p4info;
-
-class VlanPopTableTest : public ::testing::Test {
+class VlanPopTableTest : public BaseTableTest {
  protected:
-  VlanPopTableTest() : helper(p4info) {}
+  VlanPopTableTest() {}
 
-  static void SetUpTestSuite() {
-    ::util::Status status = ParseProtoFromString(P4INFO_TEXT, &p4info);
-    if (!status.ok()) {
-      std::exit(EXIT_FAILURE);
-    }
-  }
-
-  void SetUp() { helper.SelectTable("vlan_pop_mod_table"); }
+  void SetUp() { SelectTable("vlan_pop_mod_table"); }
 
   //----------------------------
-  // Utility methods
+  // Initialization
   //----------------------------
 
-  static uint32_t DecodeWordValue(const std::string& string_value) {
-    uint32_t word_value = 0;
-    for (int i = 0; i < string_value.size(); i++) {
-      word_value = (word_value << 8) | (string_value[i] & 0xff);
-    }
-    return word_value;
-  }
-
-  //----------------------------
-  // Initialization methods
-  //----------------------------
-
-  void InitAction() { helper.SelectAction("vlan_pop"); }
+  void InitAction() { SelectAction("vlan_pop"); }
 
   void InitVlanInfo() {
     // TODO(derek): vlan_id is encoded as one byte (?)
@@ -75,7 +46,7 @@ class VlanPopTableTest : public ::testing::Test {
 
   void CheckMatches() const {
     constexpr char MOD_BLOB_PTR[] = "vmeta.common.mod_blob_ptr";
-    const int MF_MOD_BLOB_PTR = helper.GetMatchFieldId(MOD_BLOB_PTR);
+    const int MF_MOD_BLOB_PTR = GetMatchFieldId(MOD_BLOB_PTR);
 
     ASSERT_NE(MF_MOD_BLOB_PTR, -1);
 
@@ -89,13 +60,12 @@ class VlanPopTableTest : public ::testing::Test {
   void CheckNoAction() const { ASSERT_FALSE(table_entry.has_action()); }
 
   void CheckTableEntry() const {
-    ASSERT_TRUE(helper.has_table());
-    EXPECT_EQ(table_entry.table_id(), helper.table_id());
+    ASSERT_TRUE(HasTable());
+    EXPECT_EQ(table_entry.table_id(), TableId());
   }
 
   void CheckVlanIdMatch(const ::p4::v1::FieldMatch& match) const {
     // TODO(derek): vlan_id (nominally bit<12>) is encoded as bit<8>.
-    // What gives?
     constexpr int VLAN_ID_SIZE = 1;
 
     ASSERT_TRUE(match.has_exact());
@@ -111,13 +81,11 @@ class VlanPopTableTest : public ::testing::Test {
   // Protected member data
   //----------------------------
 
-  ::p4::v1::TableEntry table_entry;
-  P4InfoHelper helper;
   uint16_t vlan_id;
 };
 
 //----------------------------------------------------------------------
-// PrepareSampleTableEntry()
+// Test cases
 //----------------------------------------------------------------------
 
 TEST_F(VlanPopTableTest, remove_entry) {
@@ -142,6 +110,7 @@ TEST_F(VlanPopTableTest, insert_entry) {
   PrepareVlanPopTableEntry(&table_entry, vlan_id, p4info, INSERT_ENTRY);
 
   // Assert
+  CheckTableEntry();
   CheckAction();
 }
 
