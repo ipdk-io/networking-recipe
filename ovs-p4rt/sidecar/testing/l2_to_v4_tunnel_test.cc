c900b624a55ce4531ad9b59b06fe56af03b0ae6d
Derek G Foster <derek.foster@intel.com>
2024-08-18 05:17:55 -0700

    [ovsp4rt] Revised l2_to_v4_tunnel_test to use BaseTableTest (#652)
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- ovs-p4rt/sidecar/testing/l2_to_v4_tunnel_test.cc
+++ ovs-p4rt/sidecar/testing/l2_to_v4_tunnel_test.cc
@@ -3,120 +3,29 @@
 
 // Unit test for PrepareL2ToTunnelV4().
 
+#define DUMP_JSON
+
 #include <arpa/inet.h>
 
 #include <iostream>
 #include <string>
 
-#include "absl/flags/flag.h"
-#include "google/protobuf/util/json_util.h"
+#include "base_table_test.h"
 #include "gtest/gtest.h"
 #include "logging/ovsp4rt_diag_detail.h"
 #include "ovsp4rt/ovs-p4rt.h"
 #include "ovsp4rt_private.h"
-#include "p4/config/v1/p4info.pb.h"
-#include "p4info_text.h"
-#include "stratum/lib/utils.h"
-
-ABSL_FLAG(bool, dump_json, false, "Dump output table_entry in JSON");
 
 namespace ovsp4rt {
 
-constexpr bool INSERT_ENTRY = true;
-constexpr bool REMOVE_ENTRY = false;
-
-using google::protobuf::util::JsonPrintOptions;
-using google::protobuf::util::MessageToJsonString;
-using stratum::ParseProtoFromString;
-
-static ::p4::config::v1::P4Info p4info;
-
-class L2ToV4TunnelTest : public ::testing::Test {
+class L2ToV4TunnelTest : public BaseTableTest {
  protected:
-  L2ToV4TunnelTest() { dump_json_ = absl::GetFlag(FLAGS_dump_json); }
-
-  static void SetUpTestSuite() {
-    ::util::Status status = stratum::ParseProtoFromString(P4INFO_TEXT, &p4info);
-    if (!status.ok()) {
-      std::exit(EXIT_FAILURE);
-    }
-  }
+  L2ToV4TunnelTest() {}
 
   void SetUp() { SelectTable("l2_to_tunnel_v4"); }
 
   //----------------------------
-  // P4Info lookup methods
-  //----------------------------
-
-  int GetMatchFieldId(const std::string& mf_name) const {
-    for (const auto& mf : TABLE->match_fields()) {
-      if (mf.name() == mf_name) {
-        return mf.id();
-      }
-    }
-    std::cerr << "Match Field '" << mf_name << "' not found!\n";
-    return -1;
-  }
-
-  int GetParamId(const std::string& param_name) const {
-    for (const auto& param : ACTION->params()) {
-      if (param.name() == param_name) {
-        return param.id();
-      }
-    }
-    std::cerr << "Action Param '" << param_name << "' not found!\n";
-    return -1;
-  }
-
-  void SelectAction(const std::string& action_name) {
-    for (const auto& action : p4info.actions()) {
-      const auto& pre = action.preamble();
-      if (pre.name() == action_name || pre.alias() == action_name) {
-        ACTION = &action;
-        ACTION_ID = pre.id();
-        return;
-      }
-    }
-    std::cerr << "Action '" << action_name << "' not found!\n";
-  }
-
-  void SelectTable(const std::string& table_name) {
-    for (const auto& table : p4info.tables()) {
-      const auto& pre = table.preamble();
-      if (pre.name() == table_name || pre.alias() == table_name) {
-        TABLE = &table;
-        TABLE_ID = pre.id();
-        return;
-      }
-    }
-    std::cerr << "Table '" << table_name << "' not found!\n";
-  }
-
-  //----------------------------
-  // Utility methods
-  //----------------------------
-
-  static uint32_t DecodeWordValue(const std::string& string_value) {
-    uint32_t word_value = 0;
-    for (int i = 0; i < string_value.size(); i++) {
-      word_value = (word_value << 8) | (string_value[i] & 0xff);
-    }
-    return word_value;
-  }
-
-  void DumpTableEntry() {
-    if (dump_json_) {
-      JsonPrintOptions options;
-      options.add_whitespace = true;
-      options.preserve_proto_field_names = true;
-      std::string output;
-      ASSERT_TRUE(MessageToJsonString(table_entry, &output, options).ok());
-      std::cout << output << std::endl;
-    }
-  }
-
-  //----------------------------
-  // Initialization methods
+  // Initialization
   //----------------------------
 
   void InitFdbInfo() {
@@ -139,30 +48,27 @@ class L2ToV4TunnelTest : public ::testing::Test {
   }
 
   //----------------------------
-  // Test-specific methods
+  // CheckAction()
   //----------------------------
 
   void CheckAction() const {
-    // ACTION_ID is defined (sanity check)
-    ASSERT_NE(ACTION_ID, -1);
-
     // Table entry specifies an action
     ASSERT_TRUE(table_entry.has_action());
-    auto table_action = table_entry.action();
+    const auto& table_action = table_entry.action();
 
     // Action ID is what we expect
-    auto action = table_action.action();
-    EXPECT_EQ(action.action_id(), ACTION_ID);
+    const auto& action = table_action.action();
+    EXPECT_EQ(action.action_id(), ActionId());
 
     // Only one parameter
     ASSERT_EQ(action.params_size(), 1);
 
     // Param ID is what we expect
-    auto param = action.params()[0];
+    const auto& param = action.params()[0];
     ASSERT_EQ(param.param_id(), GetParamId("dst_addr"));
 
     // Value has 4 bytes
-    auto param_value = param.value();
+    const auto& param_value = param.value();
     ASSERT_EQ(param_value.size(), 4);
 
     // Value is what we expect
@@ -170,27 +76,36 @@ class L2ToV4TunnelTest : public ::testing::Test {
     ASSERT_EQ(word_value, fdb_info.tnl_info.remote_ip.ip.v4addr.s_addr);
   }
 
+  //----------------------------
+  // CheckDetail()
+  //----------------------------
+
   void CheckDetail() const {
     // LogTableId is correct for this table
     EXPECT_EQ(detail.table_id, LOG_L2_TO_TUNNEL_V4_TABLE);
   }
 
+  //----------------------------
+  // CheckMatches()
+  //----------------------------
+
   void CheckMatches() const {
     constexpr char V4_KEY_DA[] = "hdrs.mac[vmeta.common.depth].da";
     const int MFID_DA = GetMatchFieldId(V4_KEY_DA);
+    ASSERT_NE(MFID_DA, -1);
 
     // Exactly one match field
     ASSERT_EQ(table_entry.match_size(), 1);
 
     // Match Field ID is what we expect
-    auto match = table_entry.match()[0];
+    const auto& match = table_entry.match()[0];
     ASSERT_EQ(match.field_id(), MFID_DA);
 
     // Value is what we expect
-    CheckMacAddr(match);
+    CheckMacAddrMatch(match);
   }
 
-  void CheckMacAddr(const ::p4::v1::FieldMatch& match) const {
+  void CheckMacAddrMatch(const ::p4::v1::FieldMatch& match) const {
     constexpr int MAC_ADDR_SIZE = 6;
 
     // This is an exact-match field
@@ -207,44 +122,31 @@ class L2ToV4TunnelTest : public ::testing::Test {
     }
   }
 
-  void CheckNoAction() const {
-    // Table entry does not specify an action
-    EXPECT_FALSE(table_entry.has_action());
-  }
+  //----------------------------
+  // CheckNoAction()
+  //----------------------------
+
+  void CheckNoAction() const { EXPECT_FALSE(table_entry.has_action()); }
+
+  //----------------------------
+  // CheckTableEntry()
+  //----------------------------
 
   void CheckTableEntry() const {
-    // Table is defined (sanity check)
-    ASSERT_FALSE(TABLE == nullptr);
-
-    // Table ID is what we expect
-    EXPECT_EQ(table_entry.table_id(), TABLE_ID);
+    ASSERT_TRUE(HasTable());
+    EXPECT_EQ(table_entry.table_id(), TableId());
   }
 
   //----------------------------
   // Protected member data
   //----------------------------
 
-  // Working variables
   struct mac_learning_info fdb_info = {0};
-  ::p4::v1::TableEntry table_entry;
   DiagDetail detail;
-
-  // Comparison variables
-  uint32_t TABLE_ID;
-  int ACTION_ID = -1;
-
- private:
-  //----------------------------
-  // Private member data
-  //----------------------------
-  const ::p4::config::v1::Table* TABLE = nullptr;
-  const ::p4::config::v1::Action* ACTION = nullptr;
-
-  bool dump_json_;
 };
 
 //----------------------------------------------------------------------
-// L2ToV4TunnelTest()
+// Test cases
 //----------------------------------------------------------------------
 TEST_F(L2ToV4TunnelTest, remove_entry) {
   // Arrange
@@ -256,7 +158,6 @@ TEST_F(L2ToV4TunnelTest, remove_entry) {
   DumpTableEntry();
 
   // Assert
-  ASSERT_EQ(table_entry.table_id(), TABLE_ID);
   CheckDetail();
   CheckTableEntry();
   CheckMatches();
